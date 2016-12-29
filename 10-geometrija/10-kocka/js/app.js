9180e89606790cbf26e936cfe03db8ce963d0875
Damjan Pavlica <mudroljub@gmail.com>
2016-12-29 22:18:21 +0100

    pomalo

--- 10-geometrija/10-kocka/js/app.js
+++ 10-geometrija/10-kocka/js/app.js
@@ -1,52 +1,42 @@
-var example = (function(){
+var scene = new THREE.Scene(),
+  renderer = window.WebGLRenderingContext ? new THREE.WebGLRenderer() : new THREE.CanvasRenderer(),
+  light = new THREE.AmbientLight(0xffffff),
+  camera,
+  box
 
-    "use strict";
-    
-    var scene=new THREE.Scene(),
-    renderer = window.WebGLRenderingContext ? new THREE.WebGLRenderer() : new THREE.CanvasRenderer(),
-    light= new THREE.AmbientLight(0xffffff),            
-    camera,        
-    box;
+function initScene () {
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  document.getElementById('webgl-container').appendChild(renderer.domElement)
 
-    function initScene(){
-        renderer.setSize( window.innerWidth, window.innerHeight );
-        document.getElementById("webgl-container").appendChild(renderer.domElement);
+  scene.add(light)
 
-        scene.add(light);
-                          
-        camera = new THREE.PerspectiveCamera(
-        35,
-        window.innerWidth / window.innerHeight,
-        1,
-        1000
-        );
-                            
-        camera.position.z= 100;            
-        scene.add( camera ); 
+  camera = new THREE.PerspectiveCamera(
+      35,
+      window.innerWidth / window.innerHeight,
+      1,
+      1000
+      )
 
-        box = new THREE.Mesh(
-        new THREE.BoxGeometry(20,20,20),
-        new THREE.MeshBasicMaterial({color: 0xFF0000})
-        );
+  camera.position.z = 100
+  scene.add(camera)
 
-        box.name="box";   
+  box = new THREE.Mesh(
+      new THREE.BoxGeometry(20, 20, 20),
+      new THREE.MeshBasicMaterial({color: 0xFF0000})
+      )
 
-        scene.add(box);
+  box.name = 'box'
 
-        render();
-    }
+  scene.add(box)
 
-    function render(){
-        box.rotation.y +=0.01;
-        
-        renderer.render(scene, camera); 
-        requestAnimationFrame(render);        
-    }
+  render()
+}
 
-    window.onload = initScene;
-    
-    return {
-        scene: scene
-    }
+function render () {
+  box.rotation.y += 0.01
 
-})();
+  renderer.render(scene, camera)
+  requestAnimationFrame(render)
+}
+
+window.onload = initScene
