8cb8833c33e238fbba1f2a0f895e35ff746b3772
mudroljub <mudroljub@gmail.com>
2017-01-01 20:38:38 +0100

    m

--- 40-rad-sa-modelima/35-razno-obj/js/main.js
+++ 40-rad-sa-modelima/35-razno-obj/js/main.js
@@ -17,12 +17,23 @@ document.body.appendChild(renderer.domElement)
 
 const kontrole = new THREE.OrbitControls(kamera)
 
-let loader = new THREE.OBJLoader()
-// loader.options.convertUpAxis = true
-loader.load('modeli/castle-X6.obj', object => {
-  object.scale.set(skaliranje, skaliranje, skaliranje)
-  scena.add(object)
+var mtlLoader = new THREE.MTLLoader()
+mtlLoader.load('modeli/castle/castle.mtl', function (materials) {
+  materials.preload()
+  var objLoader = new THREE.OBJLoader()
+  objLoader.setMaterials(materials)
+  objLoader.load('modeli/castle/castle.obj', function (object) {
+    object.scale.set(skaliranje, skaliranje, skaliranje)
+    scena.add(object)
+  })
 })
+
+// let loader = new THREE.OBJLoader()
+// // loader.options.convertUpAxis = true
+// loader.load('modeli/castle-X6.obj', object => {
+//   object.scale.set(skaliranje, skaliranje, skaliranje)
+//   scena.add(object)
+// })
 
 var directionalLight = new THREE.DirectionalLight(0xffeedd)
 directionalLight.position.set(0, 0, 1)
