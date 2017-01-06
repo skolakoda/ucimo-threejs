58400d194abe9190e28e9056483e90b22d8feac4
Damjan Pavlica <mudroljub@gmail.com>
2017-01-06 16:42:55 +0100

    prilagodjene putanje za windows

--- engine.js
+++ engine.js
@@ -3,17 +3,19 @@
 const fs = require('fs')
 const chokidar = require('chokidar')
 const cheerio = require('cheerio')
+const path = require('path')
 
 /** KONFIG **/
 
-const putDoFoldera = '40-rad-sa-modelima/90-modeli-dae/modeli/'
-const indexFajl = '40-rad-sa-modelima/90-modeli-dae/index.html'
+const osnovniPut = path.join('40-rad-sa-modelima', '90-modeli-dae')
+const putDoFoldera = path.join(osnovniPut, 'modeli', '/')
+const putDoFajla = path.join(osnovniPut, 'index.html')
 const chokidarOpcije = {
   depth: 0,
   ignoreInitial: true,
   persistent: true
 }
-const nadgledac = chokidar.watch(`./${putDoFoldera}`, chokidarOpcije)
+const nadgledac = chokidar.watch(putDoFoldera, chokidarOpcije)
 
 let sadrzajFajla = ''
 let $ = {}
@@ -21,29 +23,22 @@ let $ = {}
 /** FUNKCIJE **/
 
 const praviSablon = put => {
-  return (
-    `<option id="${put}" value="modeli/${put}/model.dae">${put}</option>
-    `
-  )
+  const value = path.join('modeli', put, 'model.dae')
+  return `<option id="${put}" value="${value}">${put}</option>
+  `
 }
 
-const sredjujePut = put => {
-  put = put.replace(putDoFoldera, '') // otklanja prefix
-  if (put.indexOf('/')) put = put.split('/')[0] // otklanja sufix
-  return put
-}
-
-const azuriraFajl = (put, istina) => {
-  put = sredjujePut(put)
-  if (istina) $('#probni-modeli').append(praviSablon(put))
-  else $(`#${put}`).remove()
+const azuriraFajl = (put, dodaje) => {
+  const nazivFoldera = put.replace(putDoFoldera, '')
+  if (dodaje) $('#probni-modeli').append(praviSablon(nazivFoldera))
+  else $(`#${nazivFoldera}`).remove()
   sadrzajFajla = $.html()
-  fs.writeFile(indexFajl, sadrzajFajla)
+  fs.writeFile(putDoFajla, sadrzajFajla)
 }
 
 /** INIT **/
 
-fs.readFile(`./${indexFajl}`, 'utf8', (err, data) => {
+fs.readFile(putDoFajla, 'utf8', (err, data) => {
   if (err) throw err
   sadrzajFajla = data
   $ = cheerio.load(sadrzajFajla)
