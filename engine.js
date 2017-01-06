a997145dde30ef49f7dd9933a296c04a47424404
Damjan Pavlica <mudroljub@gmail.com>
2017-01-06 05:10:27 +0100

    build engine

--- engine.js
+++ engine.js
@@ -1,22 +1,57 @@
-var chokidar = require('chokidar')
+// TODO: napraviti platformski nezavisan slash / ili \
 
-var watcher = chokidar.watch('./40-rad-sa-modelima/90-modeli-dae/modeli', {
-  // cwd: '40-rad-sa-modelima/90-modeli-dae/',
+const fs = require('fs')
+const chokidar = require('chokidar')
+const cheerio = require('cheerio')
+
+/** KONFIG **/
+
+const putDoFoldera = '40-rad-sa-modelima/90-modeli-dae/modeli/'
+const indexFajl = '40-rad-sa-modelima/90-modeli-dae/index.html'
+const nadgledac = chokidar.watch(`./${putDoFoldera}`, {
   // ignoreInitial: true,
   persistent: true
 })
 
-watcher
-  .on('addDir', putanja => {
-    putanja = putanja.replace('40-rad-sa-modelima/90-modeli-dae/modeli', '')
-    console.log(`fajl: ${putanja} je dodat.`)
-    // apendovati u padajuci meni
+let sadrzajFajla = ''
+let $
+
+/** FUNKCIJE **/
+
+const praviSablon = put => {
+  `<option value="modeli/${put}/model.dae">${put}</option>`
+}
+
+/** INIT **/
+
+fs.readFile(`./${indexFajl}`, 'utf8', (err, data) => {
+  if (err) throw err
+  sadrzajFajla = data
+  $ = cheerio.load(sadrzajFajla)
+  // console.log(sadrzajFajla)
+})
+
+/** EVENTS **/
+
+nadgledac
+  .on('addDir', put => {
+    put = put.replace(putDoFoldera, '') // otklanja prefix
+    if (put.indexOf('/')) put = put.split('/')[0] // otklanja sufix
+    console.log(praviSablon(put))
+    // apenduje padajuci meni
+    $('#test').append(praviSablon(put))
+    // azurira sadrzajFajla
+    sadrzajFajla = $.html()
+    // upisuje fajl
+    fs.writeFile(indexFajl, sadrzajFajla, function (err) {
+      if (err) return console.log(err)
+    })
   })
-  .on('unlink', putanja => {
-    console.log(`fajl: ${putanja} je obrisan.`)
-  })
-  // .on('change', function (putanja) {
-  //   console.log('File', putanja, 'has been changed')
+  // .on('unlink', put => {
+  //   console.log(`fajl: ${put} je obrisan.`)
+  // })
+  // .on('change', function (put) {
+  //   console.log('File', put, 'has been changed')
   // })
   // .on('error', function (error) {
   //   console.error('Error happened', error)
