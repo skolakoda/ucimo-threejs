e18469b47149eb0d39dcfd7194f06f34d295c91e
Damjan Pavlica <mudroljub@gmail.com>
2017-10-14 18:50:03 +0200

    pomalo

--- 70-primeri/55-robotska-ruka/js/main.js
+++ 70-primeri/55-robotska-ruka/js/main.js
@@ -1,18 +1,14 @@
 /* global THREE, TWEEN */
 // Izvor: http://www.engineering-bear.com/projects/three-js-webgl
 
-let base, mesh2, mesh3, mesh4, hand // robot parts
 const sirina = window.innerWidth
 const visina = window.innerHeight
 
-const geometrije = []
-const delovi = []
-const tweens = [{status: false}, {status: false}, {status: false}, {status: false}]
-
-const dummy = new THREE.Object3D() // base to body joint (rotation limited to y osa)
-const dummy2 = new THREE.Object3D() // body to arm1 (rotation limited to z osa)
-const dummy3 = new THREE.Object3D() // arm1 to arm2 (rotation limited to z osa)
-const dummy4 = new THREE.Object3D() // arm2 to hand (rotation limited to z osa)
+const deo1 = new THREE.Object3D() // rotation limited to y
+const deo2 = new THREE.Object3D() // rotation limited to z osa
+const deo3 = new THREE.Object3D() // rotation limited to z osa
+const deo4 = new THREE.Object3D() // rotation limited to z osa
+const delovi = [deo1, deo2, deo3, deo4]
 
 /* INIT */
 
@@ -37,97 +33,76 @@ container.appendChild(renderer.domElement)
 /* FUNCTIONS */
 
 function createBase(geometry) {
-  geometrije[0] = geometry
   const material = new THREE.MeshFaceMaterial()
-  base = new THREE.Mesh(geometry, material)
+  const base = new THREE.Mesh(geometry, material)
   base.scale.x = base.scale.y = base.scale.z = 75
-  // adding joint for body
-  base.addChild(dummy)
-  // control body location by moving joint x,y,z
-  dummy.position.y = 18
-  dummy.control = 'y' // y osa is controlled
-  // add all dummy joints to an array so i can control them easier later
-  delovi.push(dummy)
+  base.addChild(deo1)
+  deo1.position.y = 18
+  deo1.osaRotacije = 'y'
+  scene.addObject(base)
 }
 
 function createBody(geometry) {
-  geometrije[1] = geometry
   const material = new THREE.MeshFaceMaterial()
-  mesh2 = new THREE.Mesh(geometry, material)
-  dummy.addChild(mesh2)
-  // adding joint for arm1
-  dummy.addChild(dummy2)
-  dummy2.position.x = 0
-  dummy2.position.y = -8
-  dummy2.control = 'z' // z osa is controlled
-  delovi.push(dummy2)
+  const body = new THREE.Mesh(geometry, material)
+  deo1.addChild(body)
+  deo1.addChild(deo2)
+  deo2.position.x = 0
+  deo2.position.y = -8
+  deo2.osaRotacije = 'z'
 }
 
 function createArm1(geometry) {
-  geometrije[2] = geometry
   const material = new THREE.MeshFaceMaterial()
-  mesh3 = new THREE.Mesh(geometry, material)
-  dummy2.addChild(mesh3)
-  // add joint for arm 2
-  dummy2.addChild(dummy3)
-  // these offsets are set manually
-  dummy3.position.x = -14.5
-  dummy3.position.y = 13
-  dummy3.control = 'z'
-  delovi.push(dummy3)
+  const arm1 = new THREE.Mesh(geometry, material)
+  deo2.addChild(arm1)
+  deo2.addChild(deo3)
+  deo3.position.x = -14.5
+  deo3.position.y = 13
+  deo3.osaRotacije = 'z'
 }
 
 function createArm2(geometry) {
-  geometrije[3] = geometry
   const material = new THREE.MeshFaceMaterial()
-  mesh4 = new THREE.Mesh(geometry, material)
-  dummy3.addChild(mesh4)
-  dummy3.addChild(dummy4)
-  // these offsets are set manually
-  dummy4.position.x = -18.5
-  dummy4.position.y = 5.5
-  dummy4.control = 'z'
-  delovi.push(dummy4)
+  const arm2 = new THREE.Mesh(geometry, material)
+  deo3.addChild(arm2)
+  deo3.addChild(deo4)
+  deo4.position.x = -18.5
+  deo4.position.y = 5.5
+  deo4.osaRotacije = 'z'
 }
 
 function createHand(geometry) {
   const material = new THREE.MeshFaceMaterial()
-  geometrije[4] = geometry
-  hand = new THREE.Mesh(geometry, material)
-  dummy4.addChild(hand)
-  scene.addObject(base)
+  const hand = new THREE.Mesh(geometry, material)
+  deo4.addChild(hand)
 }
 
 function moveRobot() {
-  // ne pusta ako je neka animacija u toku
-  for (const i in tweens) {
-    if (tweens[i].status) return false
-  }
-
   for (const i in delovi) {
     const deo = delovi[i]
-    if (!deo.control) continue
+    if (deo.rotira) return false
+    if (!deo.osaRotacije) continue
 
-    const osa = deo.control
+    const osa = deo.osaRotacije
     const smer = Math.random() > 0.5 ? -1 : 1
     const randRotate = 4 * Math.random() * smer
     const randTime = 6000 * Math.random()
 
-    const param = {
+    const params = {
       obj: deo,
-      tween: tweens[i],
       [osa]: deo.rotation[osa],
       updateRotation() {
         this.obj.rotation[osa] = this[osa]
       }
     }
 
-    tweens[i].status = true
-    tweens[i].tween = new TWEEN.Tween(param)
+    deo.rotira = true
+    deo.tween = new TWEEN.Tween(params)
       .to({[osa]: deo.rotation[osa] + randRotate}, randTime)
-      .onUpdate(param.updateRotation)
+      .onUpdate(params.updateRotation)
       .easing(TWEEN.Easing.Exponential.InOut)
-      .onComplete(() => param.tween.status = false)
+      .onComplete(() => deo.rotira = false)
       .start()
   }
 }
