62d3426d87206b81e14a5fdb0b0e09e4286847b0
Damjan Pavlica <mudroljub@gmail.com>
2019-08-03 22:32:53 +0200

    Update main.js

--- 70-primeri/80-nemesis/main.js
+++ 70-primeri/80-nemesis/main.js
@@ -1,10 +1,5 @@
-/**
-http://www.isaacsukin.com/news/2012/06/how-build-first-person-shooter-browser-threejs-and-webglhtml5-canvas
- * Notes:
- * Coordinates are specified as (X, Y, Z) where X and Z are horizontal and Y is vertical
- */
-
-const map = [ // 1  2  3  4  5  6  7  8  9
+/* global $ */
+const map = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, ], // 0
     [1, 1, 0, 0, 0, 0, 0, 1, 1, 1, ], // 1
     [1, 1, 0, 0, 2, 0, 0, 0, 0, 1, ], // 2
@@ -17,8 +12,7 @@ const map = [ // 1  2  3  4  5  6  7  8  9
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, ], // 9
   ], mapW = map.length, mapH = map[0].length
 
-// Semi-constants
-let WIDTH = window.innerWidth,
+const WIDTH = window.innerWidth,
   HEIGHT = window.innerHeight,
   ASPECT = WIDTH / HEIGHT,
   UNITSIZE = 250,
@@ -28,100 +22,115 @@ let WIDTH = window.innerWidth,
   BULLETMOVESPEED = MOVESPEED * 5,
   NUMAI = 5,
   PROJECTILEDAMAGE = 20
-// Global vars
-let t = THREE, scene, cam, renderer, controls, clock, projector, model, skin
-let runAnim = true, mouse = { x: 0, y: 0 }, kills = 0, health = 100
-let healthCube, lastHealthPickup = 0
-/*
-var finder = new PF.AStarFinder({ // Defaults to Manhattan heuristic
-	allowDiagonal: true,
-}), grid = new PF.Grid(mapW, mapH, map);
-*/
 
-// Initialize and run on document ready
-$(document).ready(() => {
-  $('body').append('<div id="intro">Click to start</div>')
-  $('#intro').css({width: WIDTH, height: HEIGHT}).one('click', function(e) {
-    e.preventDefault()
-    $(this).fadeOut()
-    init()
-    setInterval(drawRadar, 1000)
-    animate()
-  })
-})
+const ai = []
+const mouse = { x: 0, y: 0 }
+let runAnim = true
+let kills = 0
+let health = 100
+let lastHealthPickup = 0
 
-// Setup
-function init() {
-  clock = new t.Clock() // Used in render() for controls.update()
-  projector = new t.Projector() // Used in bullet projection
-  scene = new t.Scene() // Holds all objects in the canvas
-  scene.fog = new t.FogExp2(0xD6F1FF, 0.0005) // color, density
+const clock = new THREE.Clock()
+const projector = new THREE.Projector() // Used in bullet projection
+const scene = new THREE.Scene()
 
-  // Set up camera
-  cam = new t.PerspectiveCamera(60, ASPECT, 1, 10000) // FOV, aspect, near, far
-  cam.position.y = UNITSIZE * .2
-  scene.add(cam)
+const cam = new THREE.PerspectiveCamera(60, ASPECT, 1, 10000) // FOV, aspect, near, far
+cam.position.y = UNITSIZE * .2
+scene.add(cam)
 
-  // Camera moves with mouse, flies around with WASD/arrow keys
-  controls = new t.FirstPersonControls(cam)
-  controls.movementSpeed = MOVESPEED
-  controls.lookSpeed = LOOKSPEED
-  controls.lookVertical = false // Temporary solution; play on flat surfaces only
-  controls.noFly = true
+const controls = new THREE.FirstPersonControls(cam)
+controls.movementSpeed = MOVESPEED
+controls.lookSpeed = LOOKSPEED
+controls.lookVertical = false // Temporary solution; play on flat surfaces only
+controls.noFly = true
 
-  // World objects
-  setupScene()
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(WIDTH, HEIGHT)
 
-  // Artificial Intelligence
-  setupAI()
+renderer.domElement.style.backgroundColor = '#D6F1FF' // easier to see
+document.body.appendChild(renderer.domElement)
 
-  // Handle drawing as WebGL (faster than Canvas but less supported)
-  renderer = new t.WebGLRenderer()
-  renderer.setSize(WIDTH, HEIGHT)
+const healthcube = new THREE.Mesh(
+  new THREE.CubeGeometry(30, 30, 30),
+  new THREE.MeshBasicMaterial({map: THREE.ImageUtils.loadTexture('images/health.png')})
+)
+healthcube.position.set(-UNITSIZE - 15, 35, -UNITSIZE - 15)
+scene.add(healthcube)
 
-  // Add the canvas to the document
-  renderer.domElement.style.backgroundColor = '#D6F1FF' // easier to see
-  document.body.appendChild(renderer.domElement)
+const bullets = []
+const sphereMaterial = new THREE.MeshBasicMaterial({color: 0x333333})
+const sphereGeo = new THREE.SphereGeometry(2, 6, 6)
+const aiGeo = new THREE.CubeGeometry(40, 40, 40)
 
-  // Track mouse position so we know where to shoot
-  document.addEventListener('mousemove', onDocumentMouseMove, false)
-
-  // Shoot on click
-  $(document).click(e => {
-    e.preventDefault
-    if (e.which === 1)  // Left click only
-      createBullet()
-		
-  })
-
-  // Display HUD
-  $('body').append('<canvas id="radar" width="200" height="200"></canvas>')
-  $('body').append('<div id="hud"><p>Health: <span id="health">100</span><br />Score: <span id="score">0</span></p></div>')
-  $('body').append('<div id="credits"><p>Created by <a href="http://www.isaacsukin.com/">Isaac Sukin</a> using <a href="http://mrdoob.github.com/three.js/">Three.js</a><br />WASD to move, mouse to look, click to shoot</p></div>')
-
-  // Set up "hurt" flash
-  $('body').append('<div id="hurt"></div>')
-  $('#hurt').css({width: WIDTH, height: HEIGHT, })
+function distance(x1, y1, x2, y2) {
+  return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1))
 }
 
-// Helper function for browser frames
-function animate() {
-  if (runAnim) 
-    requestAnimationFrame(animate)
-	
-  render()
+function getMapSector(v) {
+  const x = Math.floor((v.x + UNITSIZE / 2) / UNITSIZE + mapW / 2)
+  const z = Math.floor((v.z + UNITSIZE / 2) / UNITSIZE + mapW / 2)
+  return {x, z}
 }
 
-// Update and display
+// Check whether a Vector3 is inside a wall
+function checkWallCollision(v) {
+  const c = getMapSector(v)
+  return map[c.x][c.z] > 0
+}
+
+function getRandBetween(lo, hi) {
+  return parseInt(Math.floor(Math.random() * (hi - lo + 1)) + lo, 10)
+}
+
+function addAI() {
+  let x, z
+  const c = getMapSector(cam.position)
+  const aiMaterial = new THREE.MeshBasicMaterial({/* color: 0xEE3333,*/map: THREE.ImageUtils.loadTexture('images/face.png')})
+  const o = new THREE.Mesh(aiGeo, aiMaterial)
+  do {
+    x = getRandBetween(0, mapW - 1)
+    z = getRandBetween(0, mapH - 1)
+  } while (map[x][z] > 0 || (x == c.x && z == c.z))
+  x = Math.floor(x - mapW / 2) * UNITSIZE
+  z = Math.floor(z - mapW / 2) * UNITSIZE
+  o.position.set(x, UNITSIZE * 0.15, z)
+  o.health = 100
+  o.pathPos = 1
+  o.lastRandomX = Math.random()
+  o.lastRandomZ = Math.random()
+  o.lastShot = Date.now() // Higher-fidelity timers aren'THREE a big deal here.
+  ai.push(o)
+  scene.add(o)
+}
+
+function createBullet(obj) {
+  if (obj === undefined) 
+    obj = cam // eslint-disable-line
+
+  const sphere = new THREE.Mesh(sphereGeo, sphereMaterial)
+  sphere.position.set(obj.position.x, obj.position.y * 0.8, obj.position.z)
+  let vector
+  if (obj instanceof THREE.Camera) {
+    vector = new THREE.Vector3(mouse.x, mouse.y, 1)
+    projector.unprojectVector(vector, obj)
+  } else 
+    vector = cam.position.clone()
+  
+  sphere.ray = new THREE.Ray(obj.position, vector.subSelf(obj.position).normalize())
+  sphere.owner = obj
+  bullets.push(sphere)
+  scene.add(sphere)
+  return sphere
+}
+
 function render() {
   const delta = clock.getDelta(), speed = delta * BULLETMOVESPEED
   const aispeed = delta * MOVESPEED
   controls.update(delta) // Move camera
 
-  // Rotate the health cube
   healthcube.rotation.x += 0.004
   healthcube.rotation.y += 0.008
-  // Allow picking it up once per minute
+  // health once per minute
   if (Date.now() > lastHealthPickup + 60000) {
     if (distance(cam.position.x, cam.position.z, healthcube.position.x, healthcube.position.z) < 15 && health != 100) {
       health = Math.min(health + 50, 100)
@@ -132,10 +141,9 @@ function render() {
   }
   else 
     healthcube.material.wireframe = true
-	
 
   // Update bullets. Walk backwards through the list so we can remove items.
-  for (var i = bullets.length - 1; i >= 0; i--) {
+  for (let i = bullets.length - 1; i >= 0; i--) {
     const b = bullets[i], p = b.position, d = b.ray.direction
     if (checkWallCollision(p)) {
       bullets.splice(i, 1)
@@ -145,9 +153,9 @@ function render() {
     // Collide with AI
     let hit = false
     for (let j = ai.length - 1; j >= 0; j--) {
-      var a = ai[j]
+      const a = ai[j]
       const v = a.geometry.vertices[0]
-      var c = a.position
+      const c = a.position
       const x = Math.abs(v.x), z = Math.abs(v.z)
       // console.log(Math.round(p.x), Math.round(p.z), c.x, c.z, x, z);
       if (p.x < c.x + x && p.x > c.x - x &&
@@ -171,7 +179,7 @@ function render() {
       $('#hurt').fadeIn(75)
       health -= 10
       if (health < 0) health = 0
-      val = health < 25 ? '<span style="color: darkRed">' + health + '</span>' : health
+      const val = health < 25 ? '<span style="color: darkRed">' + health + '</span>' : health
       $('#health').html(val)
       bullets.splice(i, 1)
       scene.remove(b)
@@ -179,14 +187,13 @@ function render() {
     }
     if (!hit) {
       b.translateX(speed * d.x)
-      // bullets[i].translateY(speed * bullets[i].direction.y);
       b.translateZ(speed * d.z)
     }
   }
 
   // Update AI.
-  for (var i = ai.length - 1; i >= 0; i--) {
-    var a = ai[i]
+  for (let i = ai.length - 1; i >= 0; i--) {
+    const a = ai[i]
     if (a.health <= 0) {
       ai.splice(i, 1)
       scene.remove(a)
@@ -202,7 +209,7 @@ function render() {
     }
     a.translateX(aispeed * a.lastRandomX)
     a.translateZ(aispeed * a.lastRandomZ)
-    var c = getMapSector(a.position)
+    const c = getMapSector(a.position)
     if (c.x < 0 || c.x >= mapW || c.y < 0 || c.y >= mapH || checkWallCollision(a.position)) {
       a.translateX(-2 * aispeed * a.lastRandomX)
       a.translateZ(-2 * aispeed * a.lastRandomZ)
@@ -221,7 +228,7 @@ function render() {
     }
   }
 
-  renderer.render(scene, cam) // Repaint
+  renderer.render(scene, cam)
 
   // Death
   if (health <= 0) {
@@ -236,149 +243,48 @@ function render() {
   }
 }
 
-// Set up the objects in the world
 function setupScene() {
   const UNITSIZE = 250, units = mapW
 
-  // Geometry: floor
-  const floor = new t.Mesh(
-    new t.CubeGeometry(units * UNITSIZE, 10, units * UNITSIZE),
-    new t.MeshLambertMaterial({color: 0xEDCBA0, /* map: t.ImageUtils.loadTexture('images/floor-1.jpg')*/})
+  const floor = new THREE.Mesh(
+    new THREE.CubeGeometry(units * UNITSIZE, 10, units * UNITSIZE),
+    new THREE.MeshLambertMaterial({color: 0xEDCBA0, /* map: THREE.ImageUtils.loadTexture('images/floor-1.jpg')*/})
   )
   scene.add(floor)
 
-  // Geometry: walls
-  const cube = new t.CubeGeometry(UNITSIZE, WALLHEIGHT, UNITSIZE)
+  const cube = new THREE.CubeGeometry(UNITSIZE, WALLHEIGHT, UNITSIZE)
   const materials = [
-	                 new t.MeshLambertMaterial({/* color: 0x00CCAA,*/map: t.ImageUtils.loadTexture('images/wall-1.jpg')}),
-	                 new t.MeshLambertMaterial({/* color: 0xC5EDA0,*/map: t.ImageUtils.loadTexture('images/wall-2.jpg')}),
-	                 new t.MeshLambertMaterial({color: 0xFBEBCD}),
-	                 ]
+    new THREE.MeshLambertMaterial({ map: THREE.ImageUtils.loadTexture('images/wall-1.jpg')}),
+    new THREE.MeshLambertMaterial({ map: THREE.ImageUtils.loadTexture('images/wall-2.jpg')}),
+    new THREE.MeshLambertMaterial({color: 0xFBEBCD}),
+  ]
   for (let i = 0; i < mapW; i++) 
     for (let j = 0, m = map[i].length; j < m; j++) 
       if (map[i][j]) {
-        const wall = new t.Mesh(cube, materials[map[i][j] - 1])
+        const wall = new THREE.Mesh(cube, materials[map[i][j] - 1])
         wall.position.x = (i - units / 2) * UNITSIZE
         wall.position.y = WALLHEIGHT / 2
         wall.position.z = (j - units / 2) * UNITSIZE
         scene.add(wall)
       }
-	
-  // Health cube
-  healthcube = new t.Mesh(
-    new t.CubeGeometry(30, 30, 30),
-    new t.MeshBasicMaterial({map: t.ImageUtils.loadTexture('images/health.png')})
-  )
-  healthcube.position.set(-UNITSIZE - 15, 35, -UNITSIZE - 15)
-  scene.add(healthcube)
 
-  // Lighting
-  const directionalLight1 = new t.DirectionalLight(0xF7EFBE, 0.7)
+  const directionalLight1 = new THREE.DirectionalLight(0xF7EFBE, 0.7)
   directionalLight1.position.set(0.5, 1, 0.5)
   scene.add(directionalLight1)
-  const directionalLight2 = new t.DirectionalLight(0xF7EFBE, 0.5)
+  const directionalLight2 = new THREE.DirectionalLight(0xF7EFBE, 0.5)
   directionalLight2.position.set(-0.5, -1, -0.5)
   scene.add(directionalLight2)
 }
 
-var ai = []
-const aiGeo = new t.CubeGeometry(40, 40, 40)
-function setupAI() {
-  for (let i = 0; i < NUMAI; i++) 
-    addAI()
-}
-
-function addAI() {
-  const c = getMapSector(cam.position)
-  const aiMaterial = new t.MeshBasicMaterial({/* color: 0xEE3333,*/map: t.ImageUtils.loadTexture('images/face.png')})
-  const o = new t.Mesh(aiGeo, aiMaterial)
-  do {
-    var x = getRandBetween(0, mapW - 1)
-    var z = getRandBetween(0, mapH - 1)
-  } while (map[x][z] > 0 || (x == c.x && z == c.z))
-  x = Math.floor(x - mapW / 2) * UNITSIZE
-  z = Math.floor(z - mapW / 2) * UNITSIZE
-  o.position.set(x, UNITSIZE * 0.15, z)
-  o.health = 100
-  // o.path = getAIpath(o);
-  o.pathPos = 1
-  o.lastRandomX = Math.random()
-  o.lastRandomZ = Math.random()
-  o.lastShot = Date.now() // Higher-fidelity timers aren't a big deal here.
-  ai.push(o)
-  scene.add(o)
-}
-
-function getAIpath(a) {
-  const p = getMapSector(a.position)
-  do { // Cop-out
-    do {
-      var x = getRandBetween(0, mapW - 1)
-      var z = getRandBetween(0, mapH - 1)
-    } while (map[x][z] > 0 || distance(p.x, p.z, x, z) < 3)
-    var path = findAIpath(p.x, p.z, x, z)
-  } while (path.length == 0)
-  return path
-}
-
-/**
- * Find a path from one grid cell to another.
- *
- * @param sX
- *   Starting grid x-coordinate.
- * @param sZ
- *   Starting grid z-coordinate.
- * @param eX
- *   Ending grid x-coordinate.
- * @param eZ
- *   Ending grid z-coordinate.
- * @returns
- *   An array of coordinates including the start and end positions representing
- *   the path from the starting cell to the ending cell.
- */
-function findAIpath(sX, sZ, eX, eZ) {
-  const backupGrid = grid.clone()
-  const path = finder.findPath(sX, sZ, eX, eZ, grid)
-  grid = backupGrid
-  return path
-}
-
-function distance(x1, y1, x2, y2) {
-  return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1))
-}
-
-function getMapSector(v) {
-  const x = Math.floor((v.x + UNITSIZE / 2) / UNITSIZE + mapW / 2)
-  const z = Math.floor((v.z + UNITSIZE / 2) / UNITSIZE + mapW / 2)
-  return {x, z}
-}
-
-/**
- * Check whether a Vector3 overlaps with a wall.
- *
- * @param v
- *   A THREE.Vector3 object representing a point in space.
- *   Passing cam.position is especially useful.
- * @returns {Boolean}
- *   true if the vector is inside a wall; false otherwise.
- */
-function checkWallCollision(v) {
-  const c = getMapSector(v)
-  return map[c.x][c.z] > 0
-}
-
-// Radar
 function drawRadar() {
-  const c = getMapSector(cam.position), context = document.getElementById('radar').getContext('2d')
-  context.font = '10px Helvetica'
+  const c = getMapSector(cam.position)
+  const context = document.getElementById('radar').getContext('2d')
   for (let i = 0; i < mapW; i++) 
     for (let j = 0, m = map[i].length; j < m; j++) {
       let d = 0
       for (let k = 0, n = ai.length; k < n; k++) {
         const e = getMapSector(ai[k].position)
-        if (i == e.x && j == e.z) 
-          d++
-				
+        if (i == e.x && j == e.z) d++
       }
       if (i == c.x && j == c.z && d == 0) {
         context.fillStyle = '#0000FF'
@@ -405,73 +311,38 @@ function drawRadar() {
         context.fillRect(i * 20, j * 20, (i + 1) * 20, (j + 1) * 20)
       }
     }
-	
 }
 
-var bullets = []
-const sphereMaterial = new t.MeshBasicMaterial({color: 0x333333})
-const sphereGeo = new t.SphereGeometry(2, 6, 6)
-function createBullet(obj) {
-  if (obj === undefined) 
-    obj = cam
-	
-  const sphere = new t.Mesh(sphereGeo, sphereMaterial)
-  sphere.position.set(obj.position.x, obj.position.y * 0.8, obj.position.z)
-
-  if (obj instanceof t.Camera) {
-    var vector = new t.Vector3(mouse.x, mouse.y, 1)
-    projector.unprojectVector(vector, obj)
-    sphere.ray = new t.Ray(
-      obj.position,
-      vector.subSelf(obj.position).normalize()
-    )
-  }
-  else {
-    var vector = cam.position.clone()
-    sphere.ray = new t.Ray(
-      obj.position,
-      vector.subSelf(obj.position).normalize()
-    )
-  }
-  sphere.owner = obj
-
-  bullets.push(sphere)
-  scene.add(sphere)
-
-  return sphere
-}
-
-function onDocumentMouseMove(e) {
+function handleMouseMove(e) {
   e.preventDefault()
   mouse.x = (e.clientX / WIDTH) * 2 - 1
   mouse.y = - (e.clientY / HEIGHT) * 2 + 1
 }
 
-// Handle window resizing
-$(window).resize(() => {
-  WIDTH = window.innerWidth
-  HEIGHT = window.innerHeight
-  ASPECT = WIDTH / HEIGHT
-  if (cam) {
-    cam.aspect = ASPECT
-    cam.updateProjectionMatrix()
-  }
-  if (renderer) 
-    renderer.setSize(WIDTH, HEIGHT)
-	
-  $('#intro, #hurt').css({width: WIDTH, height: HEIGHT, })
-})
-
-// Stop moving around when the window is unfocused (keeps my sanity!)
-$(window).focus(() => {
-  if (controls) controls.freeze = false
-})
-$(window).blur(() => {
-  if (controls) controls.freeze = true
-})
-
-// Get a random integer between lo and hi, inclusive.
-// Assumes lo and hi are integers and lo is lower than hi.
-function getRandBetween(lo, hi) {
-  return parseInt(Math.floor(Math.random() * (hi - lo + 1)) + lo, 10)
+function setupAI() {
+  for (let i = 0; i < NUMAI; i++) addAI()
 }
+
+function animate() {
+  if (runAnim)
+    requestAnimationFrame(animate)	
+  render()
+}
+
+/* INIT */
+
+$('#intro').on('click', () => {
+  $('#intro').hide()
+  setupScene()
+  setupAI()
+  setInterval(drawRadar, 1000)
+  animate()
+})
+
+/* EVENTS */
+
+document.addEventListener('mousemove', handleMouseMove, false)
+
+document.addEventListener('click', e => {
+  if (e.which === 1) createBullet() // Left click only
+})
