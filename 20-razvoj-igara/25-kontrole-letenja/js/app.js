6d124ee8c4865b499ae69a943552ed06613a1476
Damjan Pavlica <damjan.pavlica@origo.is>
2019-08-05 15:36:45 +0200

    lint

--- 20-razvoj-igara/25-kontrole-letenja/js/app.js
+++ 20-razvoj-igara/25-kontrole-letenja/js/app.js
@@ -1,70 +1,41 @@
-(function () {
-  'use strict'
+const scene = new THREE.Scene(),
+  renderer = window.WebGLRenderingContext ? new THREE.WebGLRenderer() : new THREE.CanvasRenderer(),
+  light = new THREE.AmbientLight(0xffffff),
+  clock = new THREE.Clock()
 
-  var scene = new THREE.Scene(),
-    renderer = window.WebGLRenderingContext ? new THREE.WebGLRenderer() : new THREE.CanvasRenderer(),
-    light = new THREE.AmbientLight(0xffffff),
-    camera,
-    controls,
-    clock = new THREE.Clock(),
-    plane,
-    box
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.getElementById('webgl-container').appendChild(renderer.domElement)
 
-  function initScene () {
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    document.getElementById('webgl-container').appendChild(renderer.domElement)
+scene.add(light)
 
-    scene.add(light)
+const camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 1, 1000)
+camera.position.z = 300
+scene.add(camera)
 
-    camera = new THREE.PerspectiveCamera(
-            35,
-            window.innerWidth / window.innerHeight,
-            1,
-            1000
-        )
+const box = new THREE.Mesh(new THREE.BoxGeometry(20, 20, 20), new THREE.MeshBasicMaterial({color: 0xff0000}))
 
-    camera.position.z = 300
-    scene.add(camera)
+const texture = THREE.ImageUtils.loadTexture('teksture/grasslight-big.jpg')
+const planeMaterial = new THREE.MeshPhongMaterial({
+  map: texture,
+  side: THREE.DoubleSide
+})
 
-    box = new THREE.Mesh(new THREE.BoxGeometry(20, 20, 20), new THREE.MeshBasicMaterial({
-      color: 0xff0000
-    }))
+const plane = new THREE.Mesh(new THREE.PlaneGeometry(800, 800), planeMaterial)
+plane.rotation.x = 90 * (Math.PI / 180)
+plane.position.y = -10
+plane.name = 'plane'
+scene.add(plane)
 
-        // ground
-    var texture = THREE.ImageUtils.loadTexture('teksture/grasslight-big.jpg') // texture from three.js examples
-    var planeMaterial = new THREE.MeshPhongMaterial({
-      map: texture,
-      side: THREE.DoubleSide
-    })
+scene.add(box)
 
-    plane = new THREE.Mesh(new THREE.PlaneGeometry(800, 800), planeMaterial)
-    plane.rotation.x = 90 * (Math.PI / 180)
-    plane.position.y = -10
-    plane.name = 'plane'
-    scene.add(plane)
+const controls = new THREE.FlyControls(camera)
+controls.movementSpeed = 100
+controls.domElement = document.getElementById('webgl-container')
+controls.rollSpeed = Math.PI / 24
 
-    scene.add(box)
-
-    controls = new THREE.FlyControls(camera)
-    controls.movementSpeed = 100
-    controls.domElement = document.getElementById('webgl-container')
-    controls.rollSpeed = Math.PI / 24
-
-    render()
-  }
-
-  function render () {
-    var delta = clock.getDelta()
-
-    controls.update(delta)
-
-    renderer.render(scene, camera)
-    window.requestAnimationFrame(render)
-  }
-
-  window.onload = initScene
-
-  return {
-    scene: scene
-  }
-})()
+void function render() {
+  const delta = clock.getDelta()
+  controls.update(delta)
+  renderer.render(scene, camera)
+  window.requestAnimationFrame(render)
+}()
