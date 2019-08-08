df9d547655f10dbf22562956eb07f5a76f2f75a6
Damjan Pavlica <mudroljub@gmail.com>
2019-08-08 21:47:28 +0200

    lint

--- 35-proceduralni-svetovi/30-teren-tekstura/main.js
+++ 35-proceduralni-svetovi/30-teren-tekstura/main.js
@@ -1,66 +1,54 @@
-/* global chroma */
+/* global noise, chroma, dat */
+// https://github.com/josdirksen/essential-threejs/blob/master/chapter-05/05.02-3D-plane-from-scratch-perlin.html
 const MAX_HEIGHT = 10
-let renderer
-let scene
-let camera
-let control
 const scale = chroma.scale(['blue', 'green', 'red']).domain([0, MAX_HEIGHT])
 
-function init() {
-  scene = new THREE.Scene()
-  camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000)
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000)
 
-  renderer = new THREE.WebGLRenderer()
-  renderer.setClearColor(0x000000, 1.0)
-  renderer.setSize(window.innerWidth, window.innerHeight)
-  renderer.shadowMapEnabled = true
+const renderer = new THREE.WebGLRenderer()
+renderer.setClearColor(0x000000, 1.0)
+renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.shadowMapEnabled = true
 
-  camera.position.x = 100
-  camera.position.y = 100
-  camera.position.z = 100
-  camera.lookAt(scene.position)
+camera.position.x = 100
+camera.position.y = 100
+camera.position.z = 100
+camera.lookAt(scene.position)
 
-  const spotLight = new THREE.SpotLight(0xffffff)
-  spotLight.position.set(10, 300, 10)
+const spotLight = new THREE.SpotLight(0xffffff)
+spotLight.position.set(10, 300, 10)
+scene.add(spotLight)
+scene.add(new THREE.AmbientLight(0x252525))
 
-  scene.add(spotLight)
-
-  scene.add(new THREE.AmbientLight(0x252525))
-
-  control = new function() {
-    this.toFaceMaterial = function() {
-      const mesh = scene.getObjectByName('terrain')
-      const mat = new THREE.MeshLambertMaterial()
-      mat.vertexColors = THREE.FaceColors
-      mat.shading = THREE.NoShading
-      mesh.material = mat
-    }
-    this.toNormalMaterial = function() {
-      const mesh = scene.getObjectByName('terrain')
-      const mat = new THREE.MeshNormalMaterial()
-      mesh.material = mat
-    }
-
-    this.smoothShading = false
-    this.onSmoothShadingChange = function() {
-      const {material} = scene.getObjectByName('terrain')
-      const geom = scene.getObjectByName('terrain').geometry
-
-      if (this.object.smoothShading)
-        material.shading = THREE.SmoothShading
-      else
-        material.shading = THREE.NoShading
-
-      material.needsUpdate = true
-      geom.normalsNeedUpdate = true
-    }
-  }()
-
-  addControlGui(control)
-  document.body.appendChild(renderer.domElement)
-  create3DTerrain(140, 140, 2.5, 2.5, MAX_HEIGHT)
-  render()
+const control = {
+  smoothShading: false,
+  toFaceMaterial() {
+    const mesh = scene.getObjectByName('terrain')
+    const mat = new THREE.MeshLambertMaterial()
+    mat.vertexColors = THREE.FaceColors
+    mat.shading = THREE.NoShading
+    mesh.material = mat
+  },
+  toNormalMaterial() {
+    const mesh = scene.getObjectByName('terrain')
+    const mat = new THREE.MeshNormalMaterial()
+    mesh.material = mat
+  },
+  onSmoothShadingChange() {
+    const {material} = scene.getObjectByName('terrain')
+    const geom = scene.getObjectByName('terrain').geometry
+    material.shading = this.object.smoothShading ? THREE.SmoothShading : THREE.NoShading
+    material.needsUpdate = true
+    geom.normalsNeedUpdate = true
+  }
 }
+
+addControlGui(control)
+document.body.appendChild(renderer.domElement)
+create3DTerrain(140, 140, 2.5, 2.5, MAX_HEIGHT)
+
+/* FUNCTIONS */
 
 function create3DTerrain(width, depth, spacingX, spacingZ, height) {
   const date = new Date()
@@ -76,7 +64,6 @@ function create3DTerrain(width, depth, spacingX, spacingZ, height) {
 
   for (let z = 0; z < depth - 1; z++)
     for (let x = 0; x < width - 1; x++) {
-
       const a = x + z * width
       const b = (x + 1) + (z * width)
       const c = x + ((z + 1) * width)
@@ -119,13 +106,7 @@ function getHighPoint(geometry, face) {
   const v1 = geometry.vertices[face.a].y
   const v2 = geometry.vertices[face.b].y
   const v3 = geometry.vertices[face.c].y
-
   return Math.max(v1, v2, v3)
-}
-
-function render() {
-  renderer.render(scene, camera)
-  requestAnimationFrame(render)
 }
 
 function addControlGui(controlObject) {
@@ -135,4 +116,9 @@ function addControlGui(controlObject) {
   gui.add(controlObject, 'smoothShading').onChange(controlObject.onSmoothShadingChange)
 }
 
-window.onload = init
+/* INIT */
+
+void function render() {
+  renderer.render(scene, camera)
+  requestAnimationFrame(render)
+}()
