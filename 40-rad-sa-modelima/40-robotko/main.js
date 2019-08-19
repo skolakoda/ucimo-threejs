d64e455937a11d07fcf679d2dc300cd701d57346
Damjan Pavlica <mudroljub@gmail.com>
2019-08-19 10:52:33 +0200

    lint

--- 40-rad-sa-modelima/40-robotko/main.js
+++ 40-rad-sa-modelima/40-robotko/main.js
@@ -3,66 +3,58 @@ import * as THREE from '../../node_modules/three/build/three.module.js'
 import { GUI } from '../../node_modules/three/examples/jsm/libs/dat.gui.module.js'
 import { GLTFLoader } from '../../node_modules/three/examples/jsm/loaders/GLTFLoader.js'
 
-let container, clock, gui, mixer, actions, activeAction, previousAction
-let camera, scene, renderer, model, face
+let gui, mixer, actions, activeAction, previousAction, model, face
 
 const api = { state: 'Walking' }
 
-init()
-animate()
+const container = document.createElement('div')
+document.body.appendChild(container)
 
-function init() {
-  container = document.createElement('div')
-  document.body.appendChild(container)
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.25, 100)
+camera.position.set(- 5, 3, 10)
+camera.lookAt(new THREE.Vector3(0, 2, 0))
 
-  camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.25, 100)
-  camera.position.set(- 5, 3, 10)
-  camera.lookAt(new THREE.Vector3(0, 2, 0))
+const scene = new THREE.Scene()
+scene.background = new THREE.Color(0xe0e0e0)
+scene.fog = new THREE.Fog(0xe0e0e0, 20, 100)
 
-  scene = new THREE.Scene()
-  scene.background = new THREE.Color(0xe0e0e0)
-  scene.fog = new THREE.Fog(0xe0e0e0, 20, 100)
+const clock = new THREE.Clock()
 
-  clock = new THREE.Clock()
+let light = new THREE.HemisphereLight(0xffffff, 0x444444)
+light.position.set(0, 20, 0)
+scene.add(light)
+light = new THREE.DirectionalLight(0xffffff)
+light.position.set(0, 20, 10)
+scene.add(light)
 
-  // lights
-  let light = new THREE.HemisphereLight(0xffffff, 0x444444)
-  light.position.set(0, 20, 0)
-  scene.add(light)
-  light = new THREE.DirectionalLight(0xffffff)
-  light.position.set(0, 20, 10)
-  scene.add(light)
+// ground
+const mesh = new THREE.Mesh(new THREE.PlaneBufferGeometry(2000, 2000), new THREE.MeshPhongMaterial({ color: 0x999999, depthWrite: false }))
+mesh.rotation.x = - Math.PI / 2
+scene.add(mesh)
 
-  // ground
-  const mesh = new THREE.Mesh(new THREE.PlaneBufferGeometry(2000, 2000), new THREE.MeshPhongMaterial({ color: 0x999999, depthWrite: false }))
-  mesh.rotation.x = - Math.PI / 2
-  scene.add(mesh)
+const grid = new THREE.GridHelper(200, 40, 0x000000, 0x000000)
+grid.material.opacity = 0.2
+grid.material.transparent = true
+scene.add(grid)
 
-  const grid = new THREE.GridHelper(200, 40, 0x000000, 0x000000)
-  grid.material.opacity = 0.2
-  grid.material.transparent = true
-  scene.add(grid)
+// model
+const loader = new GLTFLoader()
+loader.load('models/RobotExpressive.glb', gltf => {
+  model = gltf.scene
+  scene.add(model)
+  createGUI(model, gltf.animations)
+}, undefined, e => {
+  console.error(e)
+})
 
-  // model
-  const loader = new GLTFLoader()
-  loader.load('models/RobotExpressive.glb', gltf => {
-    model = gltf.scene
-    scene.add(model)
-    createGUI(model, gltf.animations)
-  }, undefined, e => {
-    console.error(e)
-  })
-
-  renderer = new THREE.WebGLRenderer({ antialias: true })
-  renderer.setPixelRatio(window.devicePixelRatio)
-  renderer.setSize(window.innerWidth, window.innerHeight)
-  renderer.gammaOutput = true
-  renderer.gammaFactor = 2.2
-  container.appendChild(renderer.domElement)
-}
+const renderer = new THREE.WebGLRenderer({ antialias: true })
+renderer.setPixelRatio(window.devicePixelRatio)
+renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.gammaOutput = true
+renderer.gammaFactor = 2.2
+container.appendChild(renderer.domElement)
 
 function createGUI(model, animations) {
-
   const states = [ 'Idle', 'Walking', 'Running', 'Dance', 'Death', 'Sitting', 'Standing' ]
   const emotes = [ 'Jump', 'Yes', 'No', 'Wave', 'Punch', 'ThumbsUp' ]
 
@@ -89,7 +81,6 @@ function createGUI(model, animations) {
   statesFolder.open()
 
   // emotes
-
   const emoteFolder = gui.addFolder('Emotes')
   function createEmoteCallback(name) {
     api[ name ] = function() {
@@ -137,9 +128,9 @@ function fadeToAction(name, duration) {
     .play()
 }
 
-function animate() {
+void function animate() {
   const dt = clock.getDelta()
   if (mixer) mixer.update(dt)
   requestAnimationFrame(animate)
   renderer.render(scene, camera)
-}
+}()
