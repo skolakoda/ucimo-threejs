42d0bb962c88496c934897ba55e7eb5d4c8e39d7
Damjan Pavlica <mudroljub@gmail.com>
2019-08-29 13:23:21 +0200

    cleanup

--- 35-proceduralni-svetovi/55-drvo/Tree.js
+++ 35-proceduralni-svetovi/55-drvo/Tree.js
@@ -1,4 +1,4 @@
-/* global TWEEN */
+// http://inear.se/three_tree/
 const leafTexture = THREE.ImageUtils.loadTexture('img/leaf.png')
 const leafGeometry = new THREE.Plane(20, 20, 1, 1)
 const leafMaterial = new THREE.MeshBasicMaterial({ opacity:0.95, map: leafTexture, blending: THREE.NormalBlending, depthTest: true, transparent : true})
@@ -20,11 +20,10 @@ const branchMap = [
 const randomTable = [200, 45, 149, 271, 219, 242, 172, 269, 0, 335, 211, 223, 176, 326, 94, 306, 161, 236, 73, 286, 222, 119, 98, 33, 336, 204, 59, 81, 356, 318, 331, 183, 272, 101, 344, 251, 110, 42, 324, 169, 334, 144, 114, 9, 112, 110, 98, 127, 320, 136, 13, 200, 286, 263, 230, 164, 340, 211, 191, 33, 326, 33, 54, 128, 228, 231, 47, 46, 355, 336, 108, 348, 251, 337, 280, 80, 222, 95, 13, 79, 53, 203, 137, 267, 291, 315, 331, 50, 145, 51, 255, 58, 282, 141, 189, 310]
 let totalBranchesCreated = 0
 
-const Tree = function(materials, level, radius, entryPointIndex, maxScale) {
-
+const Tree = function(materials, radius, entryPointIndex, maxScale, level = -1) {
   THREE.Mesh.call(this, new THREE.Geometry(), materials)
 
-  level++
+  level++ // eslint-disable-line
 
   this.level = level
   this.doubleSided = false
@@ -40,25 +39,16 @@ const Tree = function(materials, level, radius, entryPointIndex, maxScale) {
   this.geometry.computeFaceNormals()
   this.geometry.computeVertexNormals()
 
-  this.geometry.__dirtyVertices = true
-  this.geometry.__dirtyNormals = true
-
   function build(scope) {
-
     let numCurrentPos = 0
     let R
     let S
-    let branchPoint
-    let height
-    let radiusStep
 
-    branchPoint = new THREE.Object3D()
-
-    height = 34 * scope.maxScale
-
-    radiusStep = scope.radius / scope.branchOffset
+    const branchPoint = new THREE.Object3D()
+    let height = 34 * scope.maxScale
+    const radiusStep = scope.radius / scope.branchOffset
     // reset
-    basePoint = new THREE.Vector3(0, 0, 0)
+    const basePoint = new THREE.Vector3(0, 0, 0)
 
     // for each step
     while (numCurrentPos < scope.branchOffset)
@@ -73,15 +63,11 @@ const Tree = function(materials, level, radius, entryPointIndex, maxScale) {
 
       if(scope.level > 0)
         branchPoint.rotation.y += 6 * TO_RADIANS
-      // branchPoint.translateX(Math.random()* 5 + 5 );
-      // branchPoint.translateY(Math.random()*5);
-
       else
         branchPoint.rotation.y += 2 * TO_RADIANS
 
       height *= 0.9
 
-      // branchPoint.rotation.z =  360 * TO_RADIANS;
       // difference from last segment
       const diffVector = new THREE.Vector3()
       diffVector.sub(branchPoint.position, basePoint)
@@ -92,29 +78,20 @@ const Tree = function(materials, level, radius, entryPointIndex, maxScale) {
       // height from transformPoint
       R = new THREE.Vector3()
       R.cross(transformPoint, diffVector)
-
       S = new THREE.Vector3()
       S.cross(R, diffVector)
-
       R.normalize()
       S.normalize()
-
       // build branch
       buildNode(numCurrentPos == 0)
-
-      // branchPoint.rotation.x = numCurrentPos * 1 * TO_RADIANS;
       branchPoint.updateMatrix()
 
       if (branchMap[scope.level][ scope.entryPointIndex + numCurrentPos]) {
-
         const totalBranches = branchMap[scope.level][ scope.entryPointIndex + numCurrentPos]
-
         let newBranchIndex = totalBranches
 
-        while (newBranchIndex > 0)
-        {
-
-          const newBranch = new Tree(scope.materials, scope.level, scope.radius, scope.entryPointIndex + numCurrentPos - 3, scope.maxScale * .9)
+        while (newBranchIndex > 0) {
+          const newBranch = new Tree(scope.materials, scope.radius, scope.entryPointIndex + numCurrentPos - 3, scope.maxScale * .9, scope.level)
           newBranch.position = branchPoint.position.clone()
           newBranch.rotation = branchPoint.rotation.clone()
           newBranch.rotation.z += randomTable[ totalBranchesCreated ] * TO_RADIANS
@@ -126,16 +103,11 @@ const Tree = function(materials, level, radius, entryPointIndex, maxScale) {
           // if random index larger than table, reset to zero
           if(totalBranchesCreated > randomTable.length - 1) totalBranchesCreated = 0
 
-          // scope.radius *= 0.95
-          // if( scope.radius < 1 ) scope.radius = 1
-
           newBranchIndex--
         }
-
       }
-      else if(numCurrentPos > scope.branchOffset - 4)
-
-        for(i = 0; i < 2; i++) {
+      else if (numCurrentPos > scope.branchOffset - 4)
+        for(let i = 0; i < 2; i++) {
           const mesh = new THREE.Mesh(leafGeometry, leafMaterial)
           mesh.doubleSided = true
           mesh.position = branchPoint.position.clone()
@@ -147,31 +119,12 @@ const Tree = function(materials, level, radius, entryPointIndex, maxScale) {
           mesh.rotation.y = Math.random() * 90 * TO_RADIANS
           mesh.rotation.z = Math.random() * 90 * TO_RADIANS
           scope.addChild(mesh)
-
-          const tweenTarget = mesh
-          const initRotY = mesh.rotation.y
-          const initRotZ = mesh.rotation.z
-          const randomTime = Math.random() * 2000 + 200
-          mesh.tween = new TWEEN.Tween(tweenTarget.rotation)
-            .to({ z: (initRotZ + 30 * Math.random()) * TO_RADIANS, y: initRotY + Math.random() * 65 * TO_RADIANS }, randomTime)
-            .easing(TWEEN.Easing.Quadratic.EaseInOut)
-            .start()
-
-          mesh.tweenBack = new TWEEN.Tween(tweenTarget.rotation, false)
-            .to({ z: initRotZ, y: initRotY }, randomTime)
-            .easing(TWEEN.Easing.Quadratic.EaseInOut)
-
-          mesh.tween.chain(mesh.tweenBack)
-          mesh.tweenBack.chain(mesh.tween)
-
         }
 
       numCurrentPos++
       scope.radius -= radiusStep
 
       function buildNode(bFirstNode) {
-
-        let segmentAngle
         let intSegmentStep
         let pX
         let pY
@@ -183,22 +136,18 @@ const Tree = function(materials, level, radius, entryPointIndex, maxScale) {
         const {faces} = scope.geometry
         const {faceVertexUvs} = scope.geometry
 
-        segmentAngle = Math.PI * 2 / _branchSegments
+        const segmentAngle = Math.PI * 2 / _branchSegments
 
         // ring 2-len
         let transformedRadius = scope.radius
-
         intSegmentStep = 0
 
-        while (intSegmentStep < _branchSegments)
-        {
+        while (intSegmentStep < _branchSegments) {
           // root node
           if (bFirstNode && scope.level == 0)
             transformedRadius = scope.radius + 15 * Math.random()
-
           else if(numCurrentPos < 2 && scope.level > 0 && scope.radius > 10)
             transformedRadius = scope.radius// + Math.random() * radius *.2;
-
           else
             transformedRadius = scope.radius + Math.random() * scope.radius * .2
 
@@ -209,18 +158,14 @@ const Tree = function(materials, level, radius, entryPointIndex, maxScale) {
           pZ = basePoint.z + transformedRadius * Math.cos(intSegmentStep * segmentAngle) * R.z + transformedRadius * Math.sin(intSegmentStep * segmentAngle) * S.z
 
           newVertex3D = new THREE.Vertex(new THREE.Vector3(pX, pY, pZ))
-
           vertices.push(newVertex3D)
-
           intSegmentStep++
         }
 
         if (bFirstNode) return
 
         intSegmentStep = 0
-        while (intSegmentStep < _branchSegments)
-        {
-
+        while (intSegmentStep < _branchSegments) {
           if (intSegmentStep < (_branchSegments - 1)) {
             // second floor
             p1 = vertices.length - _branchSegments + intSegmentStep + 1
@@ -242,15 +187,12 @@ const Tree = function(materials, level, radius, entryPointIndex, maxScale) {
 
           faces.push(new THREE.Face4(p1, p2, p3, p4))
 
-          const maxHeight = height * (scope.branchOffset + 1)
-
           const startX = 1 / _branchSegments * (intSegmentStep + 1)
           const endX = startX - 1 / _branchSegments
-
           const startY = numCurrentPos / scope.branchOffset * 3
           const endY = startY + 1 / scope.branchOffset * 3
 
-          faceVertexUvs[ 0 ].push([
+          faceVertexUvs[0].push([
             new THREE.UV(startX, endY),
             new THREE.UV(startX, startY),
             new THREE.UV(endX, startY),
