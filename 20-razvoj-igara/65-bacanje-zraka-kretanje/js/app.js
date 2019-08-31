9aab5e211742e55c52eda205a2ee48f3621175e1
Damjan Pavlica <mudroljub@gmail.com>
2019-08-31 11:48:22 +0200

    cistka

--- 20-razvoj-igara/65-bacanje-zraka-kretanje/js/app.js
+++ 20-razvoj-igara/65-bacanje-zraka-kretanje/js/app.js
@@ -1,114 +1,105 @@
-(function () {
-  'use strict'
+let scene = new THREE.Scene(),
+  renderer = window.WebGLRenderingContext ? new THREE.WebGLRenderer() : new THREE.CanvasRenderer(),
+  light = new THREE.AmbientLight(0xffffff),
+  camera,
+  objects = [],
+  plane,
+  isObjectSelected,
+  box
 
-  var scene = new THREE.Scene(),
-    renderer = window.WebGLRenderingContext ? new THREE.WebGLRenderer() : new THREE.CanvasRenderer(),
-    light = new THREE.AmbientLight(0xffffff),
-    camera,
-    objects = [],
-    plane,
-    isObjectSelected,
-    box
+function initScene() {
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  document.getElementById('webgl-container').appendChild(renderer.domElement)
 
-  function initScene () {
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    document.getElementById('webgl-container').appendChild(renderer.domElement)
+  scene.add(light)
 
-    scene.add(light)
+  camera = new THREE.PerspectiveCamera(
+    35,
+    window.innerWidth / window.innerHeight,
+    1,
+    1000
+  )
 
-    camera = new THREE.PerspectiveCamera(
-      35,
-      window.innerWidth / window.innerHeight,
-      1,
-      1000
-    )
+  camera.position.z = 450
+  camera.position.y = 50
+  scene.add(camera)
 
-    camera.position.z = 450
-    camera.position.y = 50
-    scene.add(camera)
+  // ground
+  const texture = THREE.ImageUtils.loadTexture('content/grasslight-big.jpg') // texture from three.js examples
+  const planeMaterial = new THREE.MeshPhongMaterial({
+    map: texture,
+    side: THREE.DoubleSide
+  })
 
-    // ground
-    var texture = THREE.ImageUtils.loadTexture('content/grasslight-big.jpg') // texture from three.js examples
-    var planeMaterial = new THREE.MeshPhongMaterial({
-      map: texture,
-      side: THREE.DoubleSide
+  plane = new THREE.Mesh(new THREE.PlaneGeometry(400, 400), planeMaterial)
+  plane.rotation.x = 90 * (Math.PI / 180)
+  plane.position.y = -10
+  plane.name = 'plane'
+  scene.add(plane)
+
+  objects.push(plane)
+
+  box = new THREE.Mesh(
+    new THREE.BoxGeometry(20, 20, 20),
+    new THREE.MeshBasicMaterial({
+      color: 0xFF0000
     })
+  )
 
-    plane = new THREE.Mesh(new THREE.PlaneGeometry(400, 400), planeMaterial)
-    plane.rotation.x = 90 * (Math.PI / 180)
-    plane.position.y = -10
-    plane.name = 'plane'
-    scene.add(plane)
+  box.name = 'box'
+  objects.push(box)
 
-    objects.push(plane)
+  scene.add(box)
 
-    box = new THREE.Mesh(
-      new THREE.BoxGeometry(20, 20, 20),
-      new THREE.MeshBasicMaterial({
-        color: 0xFF0000
-      })
-    )
+  render()
+}
 
-    box.name = 'box'
-    objects.push(box)
+function render() {
+  renderer.render(scene, camera)
+  window.requestAnimationFrame(render)
+}
 
-    scene.add(box)
+function onDocumentMouseDown(event) {
+  event.preventDefault()
 
-    render()
-  }
+  const projector = new THREE.Projector()
 
-  function render () {
-    renderer.render(scene, camera)
-    window.requestAnimationFrame(render)
-  }
+  const mouseClickVector = new THREE.Vector3((event.clientX / window.innerWidth) * 2 - 1, -(event.clientY / window.innerHeight) * 2 + 1, 0.5)
+  projector.unprojectVector(mouseClickVector, camera)
 
-  function onDocumentMouseDown (event) {
-    event.preventDefault()
+  const raycaster = new THREE.Raycaster(camera.position, mouseClickVector.sub(camera.position).normalize())
+  const intersects = raycaster.intersectObjects(objects)
 
-    var projector = new THREE.Projector()
-
-    var mouseClickVector = new THREE.Vector3((event.clientX / window.innerWidth) * 2 - 1, -(event.clientY / window.innerHeight) * 2 + 1, 0.5)
-    projector.unprojectVector(mouseClickVector, camera)
-
-    var raycaster = new THREE.Raycaster(camera.position, mouseClickVector.sub(camera.position).normalize())
-    var intersects = raycaster.intersectObjects(objects)
-
-    if (intersects.length > 0) {
-      for (var i = 0; i <= intersects.length - 1; i++) {
-        if (!isObjectSelected && intersects[i].object.name === 'box') {
-          intersects[0].object.material.color.setHex(Math.random() * 0xffffff)
-          isObjectSelected = true
-          return
-        } else if (isObjectSelected && intersects[i].object.name === 'plane') {
-          moveObject(intersects[i].point)
-          isObjectSelected = false
-          return
-        }
+  if (intersects.length > 0)
+    for (let i = 0; i <= intersects.length - 1; i++)
+      if (!isObjectSelected && intersects[i].object.name === 'box') {
+        intersects[0].object.material.color.setHex(Math.random() * 0xffffff)
+        isObjectSelected = true
+        return
+      } else if (isObjectSelected && intersects[i].object.name === 'plane') {
+        moveObject(intersects[i].point)
+        isObjectSelected = false
+        return
       }
-    }
-  }
 
-  function moveObject (destinationVector) {
-    var geometry = new THREE.Geometry()
-    geometry.vertices.push(box.position.clone())
-    geometry.vertices.push(destinationVector.clone())
+}
 
-    var line = new THREE.Line(geometry, new THREE.LineBasicMaterial({
-      color: 0xffffff
-    }))
+function moveObject(destinationVector) {
+  const geometry = new THREE.Geometry()
+  geometry.vertices.push(box.position.clone())
+  geometry.vertices.push(destinationVector.clone())
 
-    scene.add(line)
-    createjs.Tween.get(box.position).to({
-      x: destinationVector.x,
-      z: destinationVector.z
-    }, 500)
-  }
+  const line = new THREE.Line(geometry, new THREE.LineBasicMaterial({
+    color: 0xffffff
+  }))
 
-  window.onload = initScene
+  scene.add(line)
+  createjs.Tween.get(box.position).to({
+    x: destinationVector.x,
+    z: destinationVector.z
+  }, 500)
+}
 
-  document.addEventListener('mousedown', onDocumentMouseDown, false)
+window.onload = initScene
 
-  return {
-    scene: scene
-  }
-})()
+document.addEventListener('mousedown', onDocumentMouseDown, false)
