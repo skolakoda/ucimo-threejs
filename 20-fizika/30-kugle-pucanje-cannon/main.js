90adf87122467bf8b1955e06c385a6204b99e6ce
Damjan Pavlica <mudroljub@gmail.com>
2019-09-08 02:46:36 +0200

    m

--- 20-fizika/30-kugle-pucanje-cannon/main.js
+++ 20-fizika/30-kugle-pucanje-cannon/main.js
@@ -1,7 +1,4 @@
 /* global CANNON, THREE, PointerLockControls */
-
-// import * as THREE from '/node_modules/three/build/three.module.js'
-// import { PointerLockControls } from '/node_modules/three/examples/jsm/controls/PointerLockControls.js'
 import {camera, scene, renderer, clock} from '/utils/scene.js'
 
 camera.position.z = 5
@@ -18,17 +15,11 @@ const direction = new THREE.Vector3()
 const world = new CANNON.World()
 world.gravity.set(0, -20, 0)
 
-const sphereShape = new CANNON.Sphere(1.3)  // radius
+const playerShape = new CANNON.Sphere(1.3)  // radius
 const player = new CANNON.Body({mass: 5})
-player.addShape(sphereShape)
+player.addShape(playerShape)
 player.position.set(0, 3, 0)
 world.addBody(player)
-
-const groundShape = new CANNON.Plane()
-const ground = new CANNON.Body({mass: 0})
-ground.addShape(groundShape)
-ground.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI / 2)
-world.addBody(ground)
 
 const light = new THREE.DirectionalLight(0xffffff, 0.9)
 light.position.set(10, 30, 20)
@@ -39,34 +30,47 @@ const controls = new PointerLockControls(camera, player)
 controls.enabled = true
 scene.add(controls.getObject())
 
-const material = new THREE.MeshLambertMaterial()
+addGround()
 
-const floorGeometry = new THREE.PlaneGeometry(300, 300)
-floorGeometry.applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI / 2))
-const floor = new THREE.Mesh(floorGeometry, material)
-floor.receiveShadow = true
-scene.add(floor)
+for (let i = 0; i < 10; i++) addBox()
 
-const boxGeometry = new THREE.BoxGeometry(2, 2, 2)
-for (let i = 0; i < 10; i++) {
+/* HELPERS */
+
+function addGround() {
+  const groundShape = new CANNON.Plane()
+  const ground = new CANNON.Body({mass: 0})
+  ground.addShape(groundShape)
+  ground.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI / 2)
+  world.addBody(ground)
+
+  const floorGeometry = new THREE.PlaneGeometry(300, 300)
+  floorGeometry.applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI / 2))
+  const floor = new THREE.Mesh(floorGeometry, new THREE.MeshNormalMaterial())
+  floor.receiveShadow = true
+  scene.add(floor)
+}
+
+// TODO: napraviti zidine
+function addBox() {
   const box = new CANNON.Body({mass: 5})
-  box.addShape(new CANNON.Box(new CANNON.Vec3(1, 1, 1)))
+  const boxShape = new CANNON.Box(new CANNON.Vec3(1, 1, 1))
+  box.addShape(boxShape)
   box.position.set(
     (Math.random() - 0.5) * 20,
     1 + Math.random() - 0.5,
     (Math.random() - 0.5) * 20
   )
-  const boxMesh = new THREE.Mesh(boxGeometry, material)
-  boxMesh.position.copy(box.position)
-  boxMesh.castShadow = boxMesh.receiveShadow = true
+  const geometry = new THREE.BoxGeometry(2, 2, 2)
+  const mesh = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial())
+  mesh.position.copy(box.position)
+  mesh.castShadow = mesh.receiveShadow = true
   world.addBody(box)
+  scene.add(mesh)
   boxes.push(box)
-  scene.add(boxMesh)
-  boxMeshes.push(boxMesh)
+  boxMeshes.push(mesh)
 }
 
-/* HELPERS */
-
+// TODO: ukloniti Projector
 function updateShootDirection(targetVec) {
   const vector = targetVec
   targetVec.set(0, 0, 1)
@@ -84,14 +88,14 @@ function shootBall() {
   balls.push(ball)
   updateShootDirection(direction)
   const {x, y, z} = direction
-  ball.velocity.set(x * shootVelocity, y * shootVelocity, z * shootVelocity)
   ball.position.set(
-    player.position.x + x * (sphereShape.radius * 1.02 + ballShape.radius),
-    player.position.y + y * (sphereShape.radius * 1.02 + ballShape.radius),
-    player.position.z + z * (sphereShape.radius * 1.02 + ballShape.radius)
+    player.position.x + x,
+    player.position.y + y,
+    player.position.z + z
   )
+  ball.velocity.set(x * shootVelocity, y * shootVelocity, z * shootVelocity)
   const ballGeometry = new THREE.SphereGeometry(ballShape.radius, 32, 32)
-  const ballMesh = new THREE.Mesh(ballGeometry, material)
+  const ballMesh = new THREE.Mesh(ballGeometry, new THREE.MeshLambertMaterial())
   ballMesh.position.copy(ball.position)
   ballMesh.castShadow = ballMesh.receiveShadow = true
   scene.add(ballMesh)
@@ -104,16 +108,17 @@ void function update() {
   requestAnimationFrame(update)
   const delta = clock.getDelta()
   world.step(step)
-  balls.forEach((ball, i) => {
-    ballMeshes[i].position.copy(ball.position)
-    ballMeshes[i].quaternion.copy(ball.quaternion)
+  balls.forEach((b, i) => {
+    ballMeshes[i].position.copy(b.position)
+    ballMeshes[i].quaternion.copy(b.quaternion)
   })
-  boxes.forEach((box, i) => {
-    boxMeshes[i].position.copy(box.position)
-    boxMeshes[i].quaternion.copy(box.quaternion)
+  boxes.forEach((b, i) => {
+    boxMeshes[i].position.copy(b.position)
+    boxMeshes[i].quaternion.copy(b.quaternion)
   })
   controls.update(delta)
   renderer.render(scene, camera)
+  // time = Date.now()
 }()
 
 /* EVENTS */
