890e114b2608399e2cc4bda423c500d83d8c7c50
Damjan Pavlica <damjan.pavlica@origo.is>
2019-09-09 11:39:05 +0200

    update

--- 70-cestice/25-cestice-atom/game.js
+++ 70-cestice/25-cestice-atom/game.js
@@ -1,49 +1,27 @@
-const width = window.innerWidth
-const height = window.innerHeight
-let particleGroup,
-  particleAttributes
+import * as THREE from '/node_modules/three/build/three.module.js'
+import {scene, camera, renderer, clock, createOrbitControls} from '/utils/scene.js'
 
-const clock = new THREE.Clock()
+const totalParticles = 200
+const radiusRange = 50
 
-const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 20000)
-scene.add(camera)
+createOrbitControls()
 camera.position.set(0, 150, 400)
+scene.background = new THREE.Color(0x9999ff)
 
-const renderer = new THREE.WebGLRenderer({antialias: true})
-renderer.setSize(width, height)
-const container = document.getElementById('ThreeJS')
-container.appendChild(renderer.domElement)
+const texture = new THREE.TextureLoader().load('images/spark.png')
 
-const controls = new THREE.OrbitControls(camera, renderer.domElement)
-
-const skyBoxGeometry = new THREE.CubeGeometry(10000, 10000, 10000)
-const skyBoxMaterial = new THREE.MeshBasicMaterial({color: 0x9999ff, side: THREE.BackSide})
-const skyBox = new THREE.Mesh(skyBoxGeometry, skyBoxMaterial)
-scene.add(skyBox)
-
-const particleTexture = THREE.ImageUtils.loadTexture('images/spark.png')
-
-particleGroup = new THREE.Object3D()
-particleAttributes = {
+const particleGroup = new THREE.Object3D()
+const particleAttributes = {
   startSize: [],
   startPosition: [],
   randomness: []
 }
 
-const totalParticles = 200
-const radiusRange = 50
 for (let i = 0; i < totalParticles; i++) {
-  const spriteMaterial = new THREE.SpriteMaterial({map: particleTexture, useScreenCoordinates: false, color: 0xffffff})
-
-  const sprite = new THREE.Sprite(spriteMaterial)
-  sprite.scale.set(32, 32, 1.0) // imageWidth, imageHeight
+  const material = new THREE.SpriteMaterial({map: texture, color: 0xffffff})
+  const sprite = new THREE.Sprite(material)
+  sprite.scale.set(32, 32, 1.0)
   sprite.position.set(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5)
-  // for a cube:
-  // sprite.position.multiplyScalar( radiusRange );
-  // for a solid sphere:
-  // sprite.position.setLength( radiusRange * Math.random() );
-  // for a spherical shell:
   sprite.position.setLength(radiusRange * (Math.random() * 0.1 + 0.9))
   sprite.material.color.setHSL(Math.random(), 0.9, 0.7)
   sprite.material.blending = THREE.AdditiveBlending // "glowing" particles
@@ -58,7 +36,6 @@ scene.add(particleGroup)
 
 function update() {
   const time = 4 * clock.getElapsedTime()
-
   for (let c = 0; c < particleGroup.children.length; c++) {
     const sprite = particleGroup.children[c]
     // pulse away/towards center (individual rates of movement)
@@ -70,8 +47,9 @@ function update() {
   }
   // rotate the entire group
   particleGroup.rotation.y = time * 0.75
-  controls.update()
 }
+
+/* LOOP */
 
 void function animate() {
   requestAnimationFrame(animate)
--- 70-cestice/30-eksplozija-geometrije/game.js
+++ 70-cestice/30-eksplozija-geometrije/game.js
@@ -1,33 +1,18 @@
+import * as THREE from '/node_modules/three/build/three.module.js'
+import {scene, camera, renderer, createOrbitControls} from '/utils/scene.js'
+
+const scale = 0.02
 const avgVertexNormals = []
 const avgVertexCount = []
 
-let doExplode = true
+scene.background = new THREE.Color(0x000000)
 
-const scene = new THREE.Scene()
-
-const renderer = new THREE.WebGLRenderer()
-renderer.setClearColor(0x000000, 1.0)
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
-
-const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000)
 camera.position.x = 15
 camera.position.y = 16
 camera.position.z = 13
 camera.lookAt(scene.position)
 
-const orbit = new THREE.OrbitControls(camera)
-
-const control = new function() {
-  this.explode = function() {
-    doExplode = true
-  }
-  this.implode = function() {
-    doExplode = false
-  }
-  this.scale = 0.02
-}
-addControls(control)
+const orbit = createOrbitControls()
 
 const sphere = new THREE.BoxGeometry(4, 6, 4, 20, 20, 20)
 sphere.vertices.forEach(v => {
@@ -35,27 +20,25 @@ sphere.vertices.forEach(v => {
 })
 createParticleSystemFromGeometry(sphere)
 
-function explode(outwards) {
-  const dir = outwards === true ? 1 : -1
+function explode() {
   let count = 0
   sphere.vertices.forEach(v => {
-    v.x += (avgVertexNormals[count].x * v.velocity * control.scale) * dir
-    v.y += (avgVertexNormals[count].y * v.velocity * control.scale) * dir
-    v.z += (avgVertexNormals[count].z * v.velocity * control.scale) * dir
+    v.x += avgVertexNormals[count].x * v.velocity * scale
+    v.y += avgVertexNormals[count].y * v.velocity * scale
+    v.z += avgVertexNormals[count].z * v.velocity * scale
     count++
   })
   sphere.verticesNeedUpdate = true
 }
 
 function createParticleSystemFromGeometry(geom) {
-  const psMat = new THREE.PointCloudMaterial()
-  psMat.map = THREE.ImageUtils.loadTexture('../../assets/textures/ps_ball.png')
+  const psMat = new THREE.PointsMaterial()
+  psMat.map = new THREE.TextureLoader().load('../../assets/textures/ps_ball.png')
   psMat.blending = THREE.AdditiveBlending
   psMat.transparent = true
   psMat.opacity = 0.6
-  const ps = new THREE.PointCloud(geom, psMat)
+  const ps = new THREE.Points(geom, psMat)
   ps.sortParticles = true
-
   scene.add(ps)
 
   for (let i = 0; i < sphere.vertices.length; i++) {
@@ -81,17 +64,9 @@ function createParticleSystemFromGeometry(geom) {
     avgVertexNormals[i].divideScalar(avgVertexCount[i])
 }
 
-function addControls(controlObject) {
-  const gui = new dat.GUI()
-  gui.add(controlObject, 'explode')
-  gui.add(controlObject, 'implode')
-  gui.add(controlObject, 'scale', 0, 1).step(0.01)
-}
-
 void function render() {
   renderer.render(scene, camera)
   orbit.update()
-  if (doExplode) explode(true)
-  else explode(false)
+  explode()
   requestAnimationFrame(render)
 }()
