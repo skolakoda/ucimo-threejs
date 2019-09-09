cce6ffcde6e3973004ea0d6b3bc637a40b6d5345
Damjan Pavlica <damjan.pavlica@origo.is>
2019-09-09 11:53:41 +0200

    update

--- 70-cestice/40-sneg/game.js
+++ 70-cestice/40-sneg/game.js
@@ -1,121 +1,67 @@
-// TODO: https://threejs.org/examples/webgl_points_sprites.html
+import * as THREE from '/node_modules/three/build/three.module.js'
+import {scene, camera, renderer} from '/utils/scene.js'
 
-function initRenderer(additionalProperties) {
-  const props = (typeof additionalProperties !== 'undefined' && additionalProperties) ? additionalProperties : {}
-  const renderer = new THREE.WebGLRenderer(props)
-  renderer.shadowMap.enabled = true
-  renderer.shadowMapSoft = true
-  renderer.shadowMap.type = THREE.PCFSoftShadowMap
+const materials = []
 
-  renderer.setClearColor(new THREE.Color(0x000000))
-  renderer.setSize(window.innerWidth, window.innerHeight)
-  renderer.shadowMap.enabled = true
-  document.getElementById('webgl-output').appendChild(renderer.domElement)
+camera.position.z = 1000
+scene.fog = new THREE.FogExp2(0x000000, 0.0008)
 
-  return renderer
+const geometry = new THREE.BufferGeometry()
+const vertices = []
+const textureLoader = new THREE.TextureLoader()
+
+const sprite = textureLoader.load('/assets/textures/snowflake.png')
+
+for (let i = 0; i < 10000; i ++) {
+  const x = Math.random() * 2000 - 1000
+  const y = Math.random() * 2000 - 1000
+  const z = Math.random() * 2000 - 1000
+  vertices.push(x, y, z)
+}
+geometry.addAttribute('position', new THREE.Float32BufferAttribute(vertices, 3))
+
+const parameters = [
+  [[ 1.0, 0.2, 0.5 ], 20 ],
+  [[ 0.95, 0.1, 0.5 ], 15 ],
+  [[ 0.90, 0.05, 0.5 ], 10 ],
+  [[ 0.85, 0, 0.5 ], 8 ],
+  [[ 0.80, 0, 0.5 ], 5 ]
+]
+
+for (let i = 0; i < parameters.length; i ++) {
+  const color = parameters[i][0]
+  const size = parameters[i][1]
+
+  materials[i] = new THREE.PointsMaterial({ size, map: sprite, blending: THREE.AdditiveBlending, depthTest: false, transparent: true })
+  materials[i].color.setHSL(color[0], color[1], color[2])
+
+  const particles = new THREE.Points(geometry, materials[i])
+
+  particles.rotation.x = Math.random() * 6
+  particles.rotation.y = Math.random() * 6
+  particles.rotation.z = Math.random() * 6
+
+  scene.add(particles)
 }
 
-function initCamera(initialPosition) {
-  const position = (initialPosition !== undefined) ? initialPosition : new THREE.Vector3(-30, 40, 30)
-  const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000)
-  camera.position.copy(position)
-  camera.lookAt(new THREE.Vector3(0, 0, 0))
+/* LOOP */
 
-  return camera
-}
-
-const renderer = initRenderer()
-const scene = new THREE.Scene()
-const camera = initCamera(new THREE.Vector3(20, 40, 110))
-camera.lookAt(new THREE.Vector3(20, 30, 0))
-
-const controls = new function() {
-  this.size = 6
-  this.opacity = 0.9
-  this.color = 0xffffff
-
-  this.sizeAttenuation = true
-
-  this.redraw = function() {
-    const toRemove = []
-    scene.children.forEach(child => {
-      if (child instanceof THREE.Points)
-        toRemove.push(child)
-
-    })
-    toRemove.forEach(child => {
-      scene.remove(child)
-    })
-    createPointInstances(controls.size, controls.opacity, controls.sizeAttenuation,
-      controls.color)
+function render() {
+  const time = Date.now() * 0.00005
+  for (let i = 0; i < scene.children.length; i ++) {
+    const object = scene.children[ i ]
+    if (object instanceof THREE.Points)
+      object.rotation.y = time * (i < 4 ? i + 1 : - (i + 1))
   }
-}
-
-const gui = new dat.GUI()
-gui.add(controls, 'size', 0, 20).onChange(controls.redraw)
-gui.add(controls, 'opacity', 0, 1).onChange(controls.redraw)
-gui.addColor(controls, 'color').onChange(controls.redraw)
-gui.add(controls, 'sizeAttenuation').onChange(controls.redraw)
-
-controls.redraw()
-
-function createPointCloud(texture, size, opacity, sizeAttenuation, color) {
-  const geom = new THREE.Geometry()
-  color = new THREE.Color(color)
-  color.setHSL(color.getHSL().h,
-    color.getHSL().s,
-    (Math.random()) * color.getHSL().l)
-
-  const material = new THREE.PointsMaterial({
-    size,
-    opacity,
-    map: texture,
-    blending: THREE.AdditiveBlending,
-    depthWrite: false,
-    sizeAttenuation,
-    color
-  })
-
-  const range = 40
-  for (let i = 0; i < 150; i++) {
-    const particle = new THREE.Vector3(
-      Math.random() * range - range / 2,
-      Math.random() * range * 1.5,
-      Math.random() * range - range / 2)
-    particle.velocityY = 0.1 + Math.random() / 5
-    particle.velocityX = (Math.random() - 0.5) / 3
-    particle.velocityZ = (Math.random() - 0.5) / 3
-    geom.vertices.push(particle)
+  for (let i = 0; i < materials.length; i ++) {
+    const color = parameters[ i ][ 0 ]
+    const h = (360 * (color[ 0 ] + time) % 360) / 360
+    materials[ i ].color.setHSL(h, color[ 1 ], color[ 2 ])
   }
-
-  const system = new THREE.Points(geom, material)
-  return system
-}
-
-function createPointInstances(size, opacity, sizeAttenuation, color) {
-  const loader = new THREE.TextureLoader()
-  const texture = loader.load('../../assets/textures/snowflake.png')
-  scene.add(createPointCloud(texture, size, opacity, sizeAttenuation, color))
-}
-
-void function render() {
-  scene.children.forEach(child => {
-    if (child instanceof THREE.Points) {
-      const vertices = child.geometry.vertices
-      vertices.forEach(v => {
-        v.y -= (v.velocityY)
-        v.x -= (v.velocityX)
-        v.z -= (v.velocityZ)
-
-        if (v.y <= 0) v.y = 60
-        if (v.x <= -20 || v.x >= 20) v.velocityX *= -1
-        if (v.z <= -20 || v.z >= 20) v.velocityZ *= -1
-      })
-
-      child.geometry.verticesNeedUpdate = true
-    }
-  })
-
-  requestAnimationFrame(render)
   renderer.render(scene, camera)
+}
+
+void function animate() {
+  requestAnimationFrame(animate)
+  render()
 }()
