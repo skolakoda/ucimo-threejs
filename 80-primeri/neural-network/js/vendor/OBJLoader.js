d4a7002013ab4dc42df289c065916709f4be2c64
Damjan Pavlica <mudroljub@gmail.com>
2019-09-10 20:14:18 +0200

    update

--- 80-primeri/neural-network/js/vendor/OBJLoader.js
+++ 80-primeri/neural-network/js/vendor/OBJLoader.js
@@ -17,7 +17,7 @@ THREE.OBJLoader.prototype = {
 		var scope = this;
 
 		var loader = new THREE.XHRLoader( scope.manager );
-		loader.setCrossOrigin( this.crossOrigin );
+		// loader.setCrossOrigin( this.crossOrigin );
 		loader.load( url, function ( text ) {
 
 			onLoad( scope.parse( text ) );
