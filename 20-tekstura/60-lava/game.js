2124b3a12cea2dc682102792d9b435c7550cf262
Damjan Pavlica <mudroljub@gmail.com>
2019-09-15 00:39:37 +0200

    m

--- 20-tekstura/60-lava/game.js
+++ 20-tekstura/60-lava/game.js
@@ -4,18 +4,14 @@ import { RenderPass } from '/node_modules/three/examples/jsm/postprocessing/Rend
 import { FilmPass } from '/node_modules/three/examples/jsm/postprocessing/FilmPass.js'
 import { BloomPass } from '/node_modules/three/examples/jsm/postprocessing/BloomPass.js'
 
-let container
-let camera, scene, renderer, composer, clock
-let uniforms, mesh
+import {camera, scene, renderer, clock, createOrbitControls} from '/utils/scene.js'
 
-container = document.getElementById('container')
-camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 1, 3000)
-camera.position.z = 4
-scene = new THREE.Scene()
-clock = new THREE.Clock()
+camera.position.z = 2
+renderer.autoClear = false
+createOrbitControls()
 
 const textureLoader = new THREE.TextureLoader()
-uniforms = {
+const uniforms = {
   'fogDensity': { value: 0.45 },
   'fogColor': { value: new THREE.Vector3(0, 0, 0) },
   'time': { value: 1.0 },
@@ -25,48 +21,30 @@ uniforms = {
 }
 uniforms.texture1.value.wrapS = uniforms.texture1.value.wrapT = THREE.RepeatWrapping
 uniforms.texture2.value.wrapS = uniforms.texture2.value.wrapT = THREE.RepeatWrapping
-const size = 0.65
+
 const material = new THREE.ShaderMaterial({
   uniforms,
   vertexShader: document.getElementById('vertexShader').textContent,
   fragmentShader: document.getElementById('fragmentShader').textContent
 })
-mesh = new THREE.Mesh(new THREE.TorusBufferGeometry(size, 0.3, 30, 30), material)
-mesh.rotation.x = 0.3
+
+const mesh = new THREE.Mesh(new THREE.TorusBufferGeometry(0.65, 0.3, 30, 30), material)
 scene.add(mesh)
 
-renderer = new THREE.WebGLRenderer({ antialias: true })
-renderer.setPixelRatio(window.devicePixelRatio)
-container.appendChild(renderer.domElement)
-renderer.autoClear = false
-
+const composer = new EffectComposer(renderer)
 const renderModel = new RenderPass(scene, camera)
 const effectBloom = new BloomPass(1.25)
 const effectFilm = new FilmPass(0.35, 0.95, 2048, false)
-
-composer = new EffectComposer(renderer)
 composer.addPass(renderModel)
 composer.addPass(effectBloom)
 composer.addPass(effectFilm)
-
-onWindowResize()
-window.addEventListener('resize', onWindowResize, false)
-
-function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight
-  camera.updateProjectionMatrix()
-  renderer.setSize(window.innerWidth, window.innerHeight)
-  composer.setSize(window.innerWidth, window.innerHeight)
-}
 
 /* LOOP */
 
 void function animate() {
   requestAnimationFrame(animate)
   const delta = 5 * clock.getDelta()
-  uniforms.time.value += 0.2 * delta
-  mesh.rotation.y += 0.0125 * delta
-  mesh.rotation.x += 0.05 * delta
+  uniforms.time.value += 0.1 * delta
   renderer.clear()
   composer.render(0.01)
 }()
