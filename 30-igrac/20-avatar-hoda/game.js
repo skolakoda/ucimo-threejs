e22f342fc3fe167ba036cdf187220b7de32f2a0a
Damjan Pavlica <mudroljub@gmail.com>
2019-09-15 11:40:14 +0200

    rename

--- 30-igrac/20-avatar-hoda/game.js
+++ 30-igrac/20-avatar-hoda/game.js
@@ -16,25 +16,25 @@ scene.add(marker)
 camera.position.z = 500
 marker.add(camera)
 
-const tekstura = new THREE.MeshNormalMaterial()
+const material = new THREE.MeshNormalMaterial()
 const telo = new THREE.SphereGeometry(100)
-const avatar = new THREE.Mesh(telo, tekstura)
+const avatar = new THREE.Mesh(telo, material)
 marker.add(avatar)
 
 const ud = new THREE.SphereGeometry(50)
-const desna_ruka = new THREE.Mesh(ud, tekstura)
+const desna_ruka = new THREE.Mesh(ud, material)
 desna_ruka.position.set(-150, 0, 0)
 avatar.add(desna_ruka)
 
-const leva_ruka = new THREE.Mesh(ud, tekstura)
+const leva_ruka = new THREE.Mesh(ud, material)
 leva_ruka.position.set(150, 0, 0)
 avatar.add(leva_ruka)
 
-const desna_noga = new THREE.Mesh(ud, tekstura)
+const desna_noga = new THREE.Mesh(ud, material)
 desna_noga.position.set(70, -120, 0)
 avatar.add(desna_noga)
 
-const leva_noga = new THREE.Mesh(ud, tekstura)
+const leva_noga = new THREE.Mesh(ud, material)
 leva_noga.position.set(-70, -120, 0)
 avatar.add(leva_noga)
 
