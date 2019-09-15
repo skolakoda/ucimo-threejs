ae93d463952acbc37a0a8d387dba8fa8eb3d761c
Damjan Pavlica <mudroljub@gmail.com>
2019-09-15 15:04:37 +0200

    cistka lava avatar

--- 20-tekstura/65-lava-avatar/game.js
+++ 20-tekstura/65-lava-avatar/game.js
@@ -1,13 +1,7 @@
 import * as THREE from '/node_modules/three/build/three.module.js'
-import { EffectComposer } from '/node_modules/three/examples/jsm/postprocessing/EffectComposer.js'
-import { RenderPass } from '/node_modules/three/examples/jsm/postprocessing/RenderPass.js'
-import { FilmPass } from '/node_modules/three/examples/jsm/postprocessing/FilmPass.js'
-import { BloomPass } from '/node_modules/three/examples/jsm/postprocessing/BloomPass.js'
-
 import {camera, scene, renderer, clock, createOrbitControls} from '/utils/scene.js'
 
 camera.position.z = 200
-renderer.autoClear = false
 createOrbitControls()
 
 /* LAVA */
@@ -76,14 +70,6 @@ const material = new THREE.ShaderMaterial({
   fragmentShader
 })
 
-const composer = new EffectComposer(renderer)
-const renderModel = new RenderPass(scene, camera)
-const effectBloom = new BloomPass(1.25)
-const effectFilm = new FilmPass(0.35, 0.95, 2048, false)
-composer.addPass(renderModel)
-composer.addPass(effectBloom)
-composer.addPass(effectFilm)
-
 /* AVATAR */
 
 const telo = new THREE.SphereGeometry(100)
@@ -114,6 +100,5 @@ void function animate() {
   requestAnimationFrame(animate)
   const delta = clock.getDelta()
   uniforms.time.value += 0.8 * delta
-  renderer.clear()
-  composer.render(0.01)
+  renderer.render(scene, camera)
 }()
