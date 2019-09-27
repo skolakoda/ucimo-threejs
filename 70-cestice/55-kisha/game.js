f9bb82f4515bcd674ac75d827539f6e2b0229a0f
Damjan Pavlica <damjan.pavlica@origo.is>
2019-09-27 11:27:18 +0200

    dobra kisha

--- 70-cestice/55-kisha/game.js
+++ 70-cestice/55-kisha/game.js
@@ -1,12 +1,14 @@
 import * as THREE from '/node_modules/three/build/three.module.js'
-import {scene, camera, renderer} from '/utils/scene.js'
+import {scene, camera, renderer, createOrbitControls} from '/utils/scene.js'
 import { randomInRange } from '/utils/helpers.js'
 
 const dropsNum = 1000
 const rain = []
 
+createOrbitControls()
+
 for (let i = 0; i < dropsNum; i++) {
-  const geometry = new THREE.SphereGeometry(1)
+  const geometry = new THREE.SphereGeometry(5)
   const material = new THREE.MeshBasicMaterial({
     color: 0x9999ff,
     transparent: true,
@@ -17,6 +19,7 @@ for (let i = 0; i < dropsNum; i++) {
   drop.position.y = Math.random() * 1000 - 500
   drop.position.z = Math.random() * 1000 - 500
   drop.velocity = randomInRange(5, 10)
+  drop.scale.x = drop.scale.z = 0.1
   scene.add(drop)
   rain.push(drop)
 }
