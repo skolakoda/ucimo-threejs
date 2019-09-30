75247d4f9ef0ad6ffb2bb53fc183111fe0283321
Damjan Pavlica <damjan.pavlica@origo.is>
2019-09-30 13:50:42 +0200

    Monster.glb

---
+++ 50-rad-sa-modelima/65-monster-glb/game.js
@@ -0,0 +1,37 @@
+import * as THREE from '/node_modules/three/build/three.module.js'
+import { GLTFLoader } from '/node_modules/three/examples/jsm/loaders/GLTFLoader.js'
+import { scene, camera, renderer, createOrbitControls } from '/utils/scene.js'
+
+createOrbitControls()
+let mixer, a = 0
+let prevTime = Date.now()
+
+const light = new THREE.DirectionalLight(0xefefff, 1.5)
+light.position.set(1, 1, 1).normalize()
+scene.add(light)
+
+const loader = new GLTFLoader()
+loader.load('/assets/models/monster/glTF-Binary/Monster.glb', ({ scene: mesh, animations }) => {
+  scene.add(mesh)
+  mixer = new THREE.AnimationMixer(mesh)
+  mixer.clipAction(animations[2]).play()
+
+  document.addEventListener('click', () => {
+    const animation = animations[++a % animations.length]
+    mixer.clipAction(animation).play()
+  })
+})
+
+function render() {
+  if (mixer) {
+    const time = Date.now()
+    mixer.update((time - prevTime) * 0.001)
+    prevTime = time
+  }
+  renderer.render(scene, camera)
+}
+
+void function animate() {
+  requestAnimationFrame(animate)
+  render()
+}()
--- 70-cestice/55-kisha/game.js
+++ 70-cestice/55-kisha/game.js
@@ -2,44 +2,47 @@ import * as THREE from '/node_modules/three/build/three.module.js'
 import {scene, camera, renderer, createOrbitControls} from '/utils/scene.js'
 import { randomInRange } from '/utils/helpers.js'
 
-const drops = createRain()
-scene.add(...drops)
+class Rain {
+  constructor() {
+    this.drops = []
+    this.createDrops()
+  }
+
+  createDrops(dropsNum = 1000) {
+    for (let i = 0; i < dropsNum; i++) {
+      const geometry = new THREE.SphereGeometry(Math.random() * 5)
+      const material = new THREE.MeshBasicMaterial({
+        color: 0x9999ff,
+        transparent: true,
+        opacity: 0.6,
+      })
+      const drop = new THREE.Mesh(geometry, material)
+      drop.scale.set(0.1, 1, 0.1)
+      drop.position.x = randomInRange(-500, 500)
+      drop.position.y = randomInRange(-500, 500)
+      drop.position.z = randomInRange(-500, 500)
+      drop.velocity = randomInRange(5, 10)
+      this.drops.push(drop)
+    }
+  }
+
+  update() {
+    this.drops.forEach(drop => {
+      drop.position.y -= drop.velocity
+      if (drop.position.y < -100) drop.position.y += 1000
+    })
+  }
+}
+
+const rain = new Rain()
+scene.add(...rain.drops)
 
 createOrbitControls()
-
-/* FUNCTIONS */
-
-function createRain(dropsNum = 1000) {
-  const drops = []
-  for (let i = 0; i < dropsNum; i++) {
-    const geometry = new THREE.SphereGeometry(Math.random() * 5)
-    const material = new THREE.MeshBasicMaterial({
-      color: 0x9999ff,
-      transparent: true,
-      opacity: 0.6,
-    })
-    const drop = new THREE.Mesh(geometry, material)
-    drop.scale.set(0.1, 1, 0.1)
-    drop.position.x = randomInRange(-500, 500)
-    drop.position.y = randomInRange(-500, 500)
-    drop.position.z = randomInRange(-500, 500)
-    drop.velocity = randomInRange(5, 10)
-    drops.push(drop)
-  }
-  return drops
-}
-
-function updateRain() {
-  drops.forEach(drop => {
-    drop.position.y -= drop.velocity
-    if (drop.position.y < -100) drop.position.y += 1000
-  })
-}
 
 /* LOOP */
 
 void function render() {
   requestAnimationFrame(render)
   renderer.render(scene, camera)
-  updateRain()
+  rain.update()
 }()
