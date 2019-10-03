0ff3d1e252d8f34436f767a8d4b2cd6b1b4e88dc
Damjan Pavlica <damjan.pavlica@origo.is>
2019-10-03 15:48:25 +0200

    cleanup

--- 80-primeri/90-simulacija-sveta/classes/Entity.js
+++ 80-primeri/90-simulacija-sveta/classes/Entity.js
@@ -3,9 +3,9 @@ import Machine from '../libs/Machine.js'
 import {rndInt} from '../utils/helpers.js'
 
 export default class Entity {
-  constructor(model, position = new THREE.Vector3(rndInt(1100), 0, rndInt(1100))) {
+  constructor(model) {
     this.model = model
-    this.destination = position.clone() // new THREE.Vector3(0, 0, 0)
+    this.destination = new THREE.Vector3(rndInt(1100), 0, rndInt(1100))
     this.vel = new THREE.Vector3(0, 0, 0)
     this.rotation = new THREE.Euler(0, 0, 0)
     this.machine = new Machine()
@@ -20,27 +20,18 @@ export default class Entity {
     return this.mesh.position
   }
 
-  set pos(newPos) {
-    this.mesh.position.copy(newPos)
-  }
-
   update(delta) {
-    // rotate to target location
     const deltaX = this.destination.x - this.mesh.position.x
     const deltaZ = this.destination.z - this.mesh.position.z
+    this.mesh.rotation.y = (Math.atan2(deltaX, deltaZ))
 
     const dv = new THREE.Vector3()
     dv.subVectors(this.destination, this.mesh.position)
     dv.multiplyScalar(this.speed * .001)
     this.vel = dv
-    this.rotation.y = (Math.atan2(deltaX, deltaZ))
 
     this.mesh.position.x += this.vel.x * delta * this.timeMult
     this.mesh.position.y += this.vel.y * delta * this.timeMult
     this.mesh.position.z += this.vel.z * delta * this.timeMult
-
-    this.mesh.rotation.x = this.rotation.x
-    this.mesh.rotation.y = this.rotation.y
-    this.mesh.rotation.z = this.rotation.z
   }
 }
