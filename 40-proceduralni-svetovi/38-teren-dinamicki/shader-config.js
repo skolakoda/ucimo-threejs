4e447c82a3b049bc43df2f2d37223f6cc4e14bca
Damjan Pavlica <damjan.pavlica@origo.is>
2021-11-27 00:31:21 +0100

    separate file

---
+++ 40-proceduralni-svetovi/38-teren-dinamicki/shader-config.js
@@ -0,0 +1,73 @@
+import * as THREE from '/node_modules/three108/build/three.module.js'
+import { NormalMapShader } from '/node_modules/three108/examples/jsm/shaders/NormalMapShader.js'
+import { TerrainShader } from '/node_modules/three108/examples/jsm/shaders/TerrainShader.js'
+import { BufferGeometryUtils } from '/node_modules/three108/examples/jsm/utils/BufferGeometryUtils.js'
+import fragmentShaderNoise from './fragmentShaderNoise.js'
+import vertexShader from './vertexShader.js'
+
+const { innerWidth, innerHeight } = window
+export const mlib = {}
+
+// HEIGHT + NORMAL MAPS
+const rx = 256, ry = 256
+const pars = { minFilter: THREE.LinearFilter, magFilter: THREE.LinearFilter, format: THREE.RGBFormat }
+export const heightMap = new THREE.WebGLRenderTarget(rx, ry, pars)
+export const normalMap = new THREE.WebGLRenderTarget(rx, ry, pars)
+export const uniformsNoise = {
+  'time': { value: 1.0 },
+  'scale': { value: new THREE.Vector2(1.5, 1.5) },
+  'offset': { value: new THREE.Vector2(0, 0) }
+}
+export const uniformsNormal = THREE.UniformsUtils.clone(NormalMapShader.uniforms)
+uniformsNormal.height.value = 0.05
+uniformsNormal.resolution.value.set(rx, ry)
+uniformsNormal.heightMap.value = heightMap.texture
+
+// TEXTURES
+const textureLoader = new THREE.TextureLoader()
+const diffuseTexture1 = textureLoader.load('textures/grasslight-big.jpg')
+const diffuseTexture2 = textureLoader.load('textures/backgrounddetailed6.jpg')
+const detailTexture = textureLoader.load('textures/grasslight-big-nm.jpg')
+diffuseTexture1.wrapS = diffuseTexture1.wrapT = THREE.RepeatWrapping
+diffuseTexture2.wrapS = diffuseTexture2.wrapT = THREE.RepeatWrapping
+detailTexture.wrapS = detailTexture.wrapT = THREE.RepeatWrapping
+
+// TERRAIN SHADER
+export const uniformsTerrain = THREE.UniformsUtils.clone(TerrainShader.uniforms)
+uniformsTerrain.tNormal.value = normalMap.texture
+uniformsTerrain.uNormalScale.value = 3.5
+uniformsTerrain.tDisplacement.value = heightMap.texture
+uniformsTerrain.tDiffuse1.value = diffuseTexture1
+uniformsTerrain.tDiffuse2.value = diffuseTexture2
+uniformsTerrain.tDetail.value = detailTexture
+uniformsTerrain.enableDiffuse1.value = true
+uniformsTerrain.enableDiffuse2.value = true
+uniformsTerrain.uDisplacementScale.value = 375
+uniformsTerrain.uRepeatOverlay.value.set(6, 6)
+
+const shaders = [
+  ['heightmap', fragmentShaderNoise, vertexShader, uniformsNoise, false],
+  ['normal', NormalMapShader.fragmentShader, NormalMapShader.vertexShader, uniformsNormal, false],
+  ['terrain', TerrainShader.fragmentShader, TerrainShader.vertexShader, uniformsTerrain, true]
+]
+shaders.forEach(shader => {
+  const material = new THREE.ShaderMaterial({
+    fragmentShader: shader[1],
+    vertexShader: shader[2],
+    uniforms: shader[3],
+    lights: shader[4],
+    fog: true
+  })
+  mlib[shader[0]] = material
+})
+
+const plane = new THREE.PlaneBufferGeometry(innerWidth, innerHeight)
+export const quadTarget = new THREE.Mesh(plane, new THREE.MeshBasicMaterial({ color: 0x000000 }))
+// quadTarget.position.z = -500
+
+// TERRAIN
+const geometry = new THREE.PlaneBufferGeometry(6000, 6000, 256, 256)
+BufferGeometryUtils.computeTangents(geometry)
+export const terrain = new THREE.Mesh(geometry, mlib.terrain)
+terrain.position.set(0, -125, 0)
+terrain.rotation.x = -Math.PI / 2
