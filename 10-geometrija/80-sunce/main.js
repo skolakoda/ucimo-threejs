d9a9961bcb161f887253b2273a5a34140f7ffb30
Damjan Pavlica <damjan.pavlica@origo.is>
2022-04-01 14:44:42 +0200

    sunce

--- 10-geometrija/80-sunce/main.js
+++ 10-geometrija/80-sunce/main.js
@@ -1,112 +1,26 @@
-/* global dat */
 import * as THREE from '/node_modules/three108/build/three.module.js'
-import { OrbitControls } from '/node_modules/three108/examples/jsm/controls/OrbitControls.js'
+import { scene, camera, renderer, createOrbitControls } from '/utils/scene.js'
 import { Sky } from '/node_modules/three108/examples/jsm/objects/Sky.js'
 
-let camera, scene, renderer
+const degToRad = deg => deg * Math.PI / 180
 
-let sky, sun
+createOrbitControls()
+createSky()
 
-init()
-render()
-
-function initSky() {
-
-  // Add Sky
-  sky = new Sky()
+function createSky() {
+  const sky = new Sky()
   sky.scale.setScalar(450000)
   scene.add(sky)
 
-  sun = new THREE.Vector3()
-
-  // / GUI
-
-  const effectController = {
-    turbidity: 10,
-    rayleigh: 3,
-    mieCoefficient: 0.005,
-    mieDirectionalG: 0.7,
-    elevation: 2,
-    azimuth: 180,
-    exposure: renderer.toneMappingExposure
-  }
-
-  function degToRad(degrees)
-  {
-    const pi = Math.PI
-    return degrees * (pi / 180)
-  }
-
-  function guiChanged() {
-
-    const { uniforms } = sky.material
-    uniforms.turbidity.value = effectController.turbidity
-    uniforms.rayleigh.value = effectController.rayleigh
-    uniforms.mieCoefficient.value = effectController.mieCoefficient
-    uniforms.mieDirectionalG.value = effectController.mieDirectionalG
-
-    const phi = degToRad(90 - effectController.elevation)
-    const theta = degToRad(effectController.azimuth)
-
-    sun.setFromSphericalCoords(1, phi, theta)
-
-    uniforms.sunPosition.value.copy(sun)
-
-    renderer.toneMappingExposure = effectController.exposure
-    renderer.render(scene, camera)
-
-  }
-
-  const gui = new dat.GUI()
-
-  gui.add(effectController, 'turbidity', 0.0, 20.0, 0.1).onChange(guiChanged)
-  gui.add(effectController, 'rayleigh', 0.0, 4, 0.001).onChange(guiChanged)
-  gui.add(effectController, 'mieCoefficient', 0.0, 0.1, 0.001).onChange(guiChanged)
-  gui.add(effectController, 'mieDirectionalG', 0.0, 1, 0.001).onChange(guiChanged)
-  gui.add(effectController, 'elevation', 0, 90, 0.1).onChange(guiChanged)
-  gui.add(effectController, 'azimuth', - 180, 180, 0.1).onChange(guiChanged)
-  gui.add(effectController, 'exposure', 0, 1, 0.0001).onChange(guiChanged)
-
-  guiChanged()
+  const sun = new THREE.Vector3()
+  const phi = degToRad(90 - 2)
+  const theta = degToRad(180)
+  sun.setFromSphericalCoords(1, phi, theta)
+  const { uniforms } = sky.material
+  uniforms.sunPosition.value.copy(sun)
 }
 
-function init() {
-
-  camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 100, 2000000)
-  camera.position.set(0, 100, 2000)
-
-  scene = new THREE.Scene()
-
-  const helper = new THREE.GridHelper(10000, 2, 0xffffff, 0xffffff)
-  scene.add(helper)
-
-  renderer = new THREE.WebGLRenderer()
-  renderer.setPixelRatio(window.devicePixelRatio)
-  renderer.setSize(window.innerWidth, window.innerHeight)
-  renderer.outputEncoding = THREE.sRGBEncoding
-  renderer.toneMapping = THREE.ACESFilmicToneMapping
-  renderer.toneMappingExposure = 0.5
-  document.body.appendChild(renderer.domElement)
-
-  const controls = new OrbitControls(camera, renderer.domElement)
-  controls.addEventListener('change', render)
-  // controls.maxPolarAngle = Math.PI / 2;
-  controls.enableZoom = false
-  controls.enablePan = false
-
-  initSky()
-
-  window.addEventListener('resize', onWindowResize)
-
-}
-
-function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight
-  camera.updateProjectionMatrix()
-  renderer.setSize(window.innerWidth, window.innerHeight)
-  render()
-}
-
-function render() {
+void function animate() {
   renderer.render(scene, camera)
-}
+  requestAnimationFrame(animate)
+}()
