fb8076e82f8c9761049e91f2b532e0f9c9121d29
Damjan Pavlica <damjan.pavlica@origo.is>
2022-04-07 23:37:08 +0200

    grad

--- 40-proceduralni-svetovi/60-proceduralni-grad/main.js
+++ 40-proceduralni-svetovi/60-proceduralni-grad/main.js
@@ -1,6 +1,6 @@
 import * as THREE from '/node_modules/three108/build/three.module.js'
 import { scene, camera, renderer, createOrbitControls, addLights } from '/utils/scene.js'
-import { randomInRange } from '/utils/helpers.js'
+import { randomGrey, generateCityTexture } from '/utils/helpers.js'
 
 addLights()
 createOrbitControls()
@@ -11,14 +11,15 @@ const plane = new THREE.Mesh(new THREE.PlaneGeometry(2000, 2000), new THREE.Mesh
 plane.rotation.x = -90 * Math.PI / 180
 scene.add(plane)
 
-const texture = generateTexture()
+const city = generateBuildings(10000)
+scene.add(city)
 
 function createBuilding() {
   const geometry = new THREE.BoxGeometry(1, 1, 1)
   geometry.applyMatrix(new THREE.Matrix4().makeTranslation(0, 0.5, 0))
 
   // roof color
-  const color = new THREE.Color(randomInRange(.7, 1), randomInRange(.7, 1), randomInRange(.7, 1))
+  const color = new THREE.Color(randomGrey())
   geometry.faces[5].color = color
   geometry.faces[4].color = color
 
@@ -36,6 +37,7 @@ function createBuilding() {
   building.rotation.y = Math.random()
   building.scale.x = building.scale.z = Math.random() * Math.random() * Math.random() * Math.random() * 50 + 10
   building.scale.y = (Math.random() * Math.random() * Math.random() * building.scale.x) * 8 + 8
+
   building.updateMatrix() // needed for merge
   return building
 }
@@ -46,41 +48,10 @@ function generateBuildings(num = 10000) {
     const building = createBuilding()
     geometry.merge(building.geometry, building.matrix)
   }
+  const texture = generateCityTexture()
   const material = new THREE.MeshLambertMaterial({ map: texture, vertexColors: THREE.FaceColors })
   return new THREE.Mesh(geometry, material)
 }
-
-function generateTexture() {
-  // beli kvadrat
-  const canvas = document.createElement('canvas')
-  canvas.width = 32
-  canvas.height = 64
-  const context = canvas.getContext('2d')
-  context.fillStyle = '#ffffff'
-  context.fillRect(0, 0, 32, 64)
-  // crno-sive nijanse
-  for (let y = 2; y < 64; y += 2)
-    for (let x = 0; x < 32; x += 2) {
-      const value = Math.floor(Math.random() * 64)
-      context.fillStyle = `rgb(${value}, ${value}, ${value})`
-      context.fillRect(x, y, 2, 1)
-    }
-
-  const canvas2 = document.createElement('canvas')
-  canvas2.width = 512
-  canvas2.height = 1024
-  const context2 = canvas2.getContext('2d')
-  context2.imageSmoothingEnabled = false
-  context2.drawImage(canvas, 0, 0, canvas2.width, canvas2.height)
-
-  const texture = new THREE.Texture(canvas2)
-  texture.needsUpdate = true
-  return texture
-}
-
-const city = generateBuildings(10000)
-
-scene.add(city)
 
 /* INIT */
 
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,5 +1,40 @@
+import * as THREE from '/node_modules/three108/build/three.module.js'
+
 /* including min, not including max */
 export function randomInRange(min, max, round = false) {
   const rand = Math.random() * (max - min) + min
   return round ? Math.floor(rand) : rand
+}
+
+export function randomGrey(min = 75, max = 150) {
+  const v = (randomInRange(min, max) | 0).toString(16)
+  return '#' + v + v + v
+}
+
+export function generateCityTexture() {
+  // beli kvadrat
+  const canvas = document.createElement('canvas')
+  canvas.width = 32
+  canvas.height = 64
+  const context = canvas.getContext('2d')
+  context.fillStyle = '#ffffff'
+  context.fillRect(0, 0, 32, 64)
+  // crno-sive nijanse
+  for (let y = 2; y < 64; y += 2)
+    for (let x = 0; x < 32; x += 2) {
+      const value = Math.floor(Math.random() * 64)
+      context.fillStyle = `rgb(${value}, ${value}, ${value})`
+      context.fillRect(x, y, 2, 1)
+    }
+
+  const canvas2 = document.createElement('canvas')
+  canvas2.width = 512
+  canvas2.height = 1024
+  const context2 = canvas2.getContext('2d')
+  context2.imageSmoothingEnabled = false
+  context2.drawImage(canvas, 0, 0, canvas2.width, canvas2.height)
+
+  const texture = new THREE.Texture(canvas2)
+  texture.needsUpdate = true
+  return texture
 }
