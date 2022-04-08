5b973c91b20c436374565a791e22b01e265e2cb9
Damjan Pavlica <damjan.pavlica@origo.is>
2022-04-08 22:06:09 +0200

    rename

--- 40-proceduralni-svetovi/64-grad-normal-map/game.js
+++ 40-proceduralni-svetovi/64-grad-normal-map/game.js
@@ -4,44 +4,32 @@ import { randomInRange } from '/utils/helpers.js'
 
 const space = 60
 const height = 10
-const cubesNum = 2000
-const shouldMerge = true // mnogo sporije renderuje sa puno objekata koji nisu spojeni
+const buildingNum = 2000
 
 const textureLoader = new THREE.TextureLoader()
 
 camera.position.set(25, 25, 25)
 createOrbitControls()
 
-if (shouldMerge) scene.add(createMergedCubes())
-else for (let i = 0; i < cubesNum; i++) scene.add(createCube())
+scene.add(createMergedCubes())
 
 /* FUNCTIONS */
 
 function createMergedCubes() {
   const parent = new THREE.Geometry()
-  for (let i = 0; i < cubesNum; i++) parent.merge(createCubeGeometry())
+  for (let i = 0; i < buildingNum; i++) parent.merge(createBuilding())
   const normalMap = textureLoader.load('/assets/textures/RooftilesWood.jpg')
   const material = new THREE.MeshNormalMaterial({ normalMap })
   return new THREE.Mesh(parent, material)
 }
 
-function createCubeGeometry() {
+function createBuilding() {
   const y = height * Math.random()
   const geometry = new THREE.BoxGeometry(4 * Math.random(), y, 4 * Math.random())
   const translation = new THREE.Matrix4().makeTranslation(randomInRange(-space, space), y / 2, randomInRange(-space, space))
   geometry.applyMatrix(translation)
   if (Math.random() > .8) geometry.rotateY(Math.random())
   return geometry
-}
-
-function createCube() {
-  const y = height * Math.random()
-  const geometry = new THREE.BoxGeometry(4 * Math.random(), y, 4 * Math.random())
-  const material = new THREE.MeshNormalMaterial()
-  material.transparent = true
-  const mesh = new THREE.Mesh(geometry, material)
-  mesh.position.set(randomInRange(-space, space), y / 2, randomInRange(-space, space))
-  return mesh
 }
 
 /* LOOP */
