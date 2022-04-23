9b8896fa0d4e132fde4d7f81f4c9c3afc13f2c87
Damjan Pavlica <damjan.pavlica@origo.is>
2022-04-23 14:19:52 +0200

    lampe savrseno

--- 40-proceduralni-svetovi/66-ulicna-rasveta/game.js
+++ 40-proceduralni-svetovi/66-ulicna-rasveta/game.js
@@ -1,15 +1,15 @@
 // https://threejs.org/examples/#webgl_lights_spotlight
 import * as THREE from '/node_modules/three108/build/three.module.js'
 import { scene, camera, renderer, createOrbitControls } from '/utils/scene.js'
-import { randomInCircle, createFloor } from '/utils/helpers.js'
+import { randomInCircle, randomInRange, createFloor } from '/utils/helpers.js'
 
-const numLampposts = 10
+const numLampposts = 15
 const size = 200
 
 camera.position.set(160, 40, 10)
 createOrbitControls()
 
-scene.add(createFloor({ size }))
+scene.add(createFloor({ size: size * 1.1 }))
 
 const ambient = new THREE.AmbientLight(0xffffff, 0.1)
 scene.add(ambient)
@@ -22,12 +22,15 @@ for (let i = 0; i < numLampposts; i++) {
 
 /* FUNCTIONS */
 
-function createLamppost({ x, z, height = 40, color = 0xdceff5 } = {}) {
+function createLamppost({ x, z, height = 40 } = {}) {
   const group = new THREE.Group()
-  const sphereGeometry = new THREE.SphereGeometry(1, 12, 16)
+
+  const sphereGeometry = new THREE.SphereGeometry(2, 12, 16)
+  const colors = [0xF5F5DC, 0xdceff5, 0xFFFF8F, 0xFFFDD0]
+  const color = colors[Math.floor(Math.random() * colors.length)]
   const sphereMaterial = new THREE.MeshBasicMaterial({ color })
   const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial)
-  sphere.position.set(x + 1, height, z)
+  sphere.position.set(x, height, z)
   group.add(sphere)
 
   const cylinderGeometry = new THREE.CylinderGeometry(.5, .5, height, 6)
@@ -41,11 +44,12 @@ function createLamppost({ x, z, height = 40, color = 0xdceff5 } = {}) {
   lamppost.position.set(x, height, z)
   lamppost.target.position.set(x, 0, z)
   lamppost.target.updateMatrixWorld()
-  // group.add(lamppost.target)
-  lamppost.angle = Math.PI / 6
-  lamppost.intensity = .8 // 0-2
-  lamppost.penumbra = 0.3
-  lamppost.distance = 200
+
+  lamppost.angle = randomInRange(Math.PI / 6, Math.PI / 3)
+  lamppost.intensity = randomInRange(.5, 2) // 1.8 // 0-2
+  lamppost.penumbra = 0.5
+  lamppost.distance = height * 2
+
   lamppost.castShadow = true
   group.add(lamppost)
 
