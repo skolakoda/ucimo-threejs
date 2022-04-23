1aaabc5849bdc0916ffb241d4233a8a39bbb9918
Damjan Pavlica <damjan.pavlica@origo.is>
2022-04-23 12:15:09 +0200

    m

--- 40-proceduralni-svetovi/66-ulicna-rasveta/game.js
+++ 40-proceduralni-svetovi/66-ulicna-rasveta/game.js
@@ -4,6 +4,15 @@ import * as THREE from '/node_modules/three108/build/three.module.js'
 import { scene, camera, renderer, createOrbitControls } from '/utils/scene.js'
 import { randomInCircle, createFloor } from '/utils/helpers.js'
 
+const params = {
+  'light color': 0xF5F5DC, // spotLight.color.getHex(),
+  intensity: 1, // 0-2 spotLight.intensity,
+  distance: 200, // spotLight.distance,
+  angle: Math.PI / 4, // spotLight.angle,
+  penumbra: 0.4, // spotLight.penumbra,
+  decay: 2, // spotLight.decay,
+}
+
 const gui = new dat.GUI()
 
 camera.position.set(160, 40, 10)
@@ -13,19 +22,18 @@ createOrbitControls()
 const ambient = new THREE.AmbientLight(0xffffff, 0.1)
 scene.add(ambient)
 
-const spotLight = new THREE.SpotLight(0xffffff, 1)
+const spotLight = new THREE.SpotLight(params.color, 1)
 spotLight.position.set(15, 40, 35)
-spotLight.angle = Math.PI / 4
-spotLight.penumbra = 0.1
-spotLight.decay = 2
-spotLight.distance = 200
+spotLight.angle = params.angle
+spotLight.penumbra = params.penumbra
+spotLight.decay = params.decay
+spotLight.distance = params.distance
 
 spotLight.castShadow = true
 spotLight.shadow.mapSize.width = 512
 spotLight.shadow.mapSize.height = 512
 spotLight.shadow.camera.near = 10
 spotLight.shadow.camera.far = 200
-spotLight.shadow.focus = 1
 scene.add(spotLight)
 
 const shadowHelper = new THREE.CameraHelper(spotLight.shadow.camera)
@@ -40,42 +48,23 @@ mesh.receiveShadow = true
 scene.add(mesh)
 
 function buildGui() {
-  const params = {
-    'light color': spotLight.color.getHex(),
-    intensity: spotLight.intensity,
-    distance: spotLight.distance,
-    angle: spotLight.angle,
-    penumbra: spotLight.penumbra,
-    decay: spotLight.decay,
-    focus: spotLight.shadow.focus
-  }
-
   gui.addColor(params, 'light color').onChange(val => {
     spotLight.color.setHex(val)
   })
-
   gui.add(params, 'intensity', 0, 2).onChange(val => {
     spotLight.intensity = val
   })
-
   gui.add(params, 'distance', 50, 200).onChange(val => {
     spotLight.distance = val
   })
-
   gui.add(params, 'angle', 0, Math.PI / 3).onChange(val => {
     spotLight.angle = val
   })
-
   gui.add(params, 'penumbra', 0, 1).onChange(val => {
     spotLight.penumbra = val
   })
-
   gui.add(params, 'decay', 1, 2).onChange(val => {
     spotLight.decay = val
-  })
-
-  gui.add(params, 'focus', 0, 1).onChange(val => {
-    spotLight.shadow.focus = val
   })
   gui.open()
 }
